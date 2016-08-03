eefe10dd72451b29096f9d201ff0765e341fd513
Punit kumar <punit.kumar1661@gmail.com>
2016-08-03 22:52:25 +0530

    env variables added

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$DB_NAME = 'alumnimeet';//getenv('DB_NAME');
-$DB_HOST = 'localhost';//getenv('DB_HOST');
-$DB_USER = 'root';//getenv('DB_USERNAME');
-$DB_PASS = '';//getenv('DB_PASSWORD');
+$DB_NAME = getenv('DB_NAME');
+$DB_HOST = getenv('DB_HOST');
+$DB_USER = getenv('DB_USERNAME');
+$DB_PASS = getenv('DB_PASSWORD');
 $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($connection->connect_error) {
   // die("Connecton failed: ".$connection->connect_error);
