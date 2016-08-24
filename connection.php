a55f238747236f7ce21f3af5aab02a576b845683
Utsav Agarwal <uagarwal744@gmail.com>
2016-08-24 16:40:49 +0530

    changing environment variables

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$DB_NAME = getenv('DB_NAME');
-$DB_HOST = getenv('DB_HOST');
-$DB_USER = getenv('DB_USERNAME');
-$DB_PASS = getenv('DB_PASSWORD');
+$DB_NAME = getenv('SAC_DB_NAME');
+$DB_HOST = getenv('SAC_DB_HOST');
+$DB_USER = getenv('SAC_DB_USERNAME');
+$DB_PASS = getenv('SAC_DB_PASSWORD');
 $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($connection->connect_error) {
   // die("Connecton failed: ".$connection->connect_error);
