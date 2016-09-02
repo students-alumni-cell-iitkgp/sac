597717fe1d23f7c24245edf70950704850dca9d6
Adarsh kashyap <idealadarshkashyap@gmail.com>
2016-09-02 21:52:07 +0530

    Update index.php
    special character

--- index.php
+++ index.php
@@ -113,7 +113,7 @@
     </div>
     <div class="row " >
     <div class="z-depth-2 about">
-      <p>The Students' Alumni Cell, IIT Kharagpur is a voluntary student body working under the aegis 
+      <p>The Students&#8217; Alumni Cell, IIT Kharagpur is a voluntary student body working under the aegis 
       of the Office of Alumni Affairs and International Relations. 
       It strives to bridge the gap between IIT Kharagpur and its esteemed alumni community
       . Over the course of past few years, the Students' Alumni Cell has emerged as a key organ of the Office of Alumni Affairs and International Relations in executing its yearlong events and activities. The Students' Alumni Cell continuously endeavours to strengthen the relations of the alumni of this institute and their alma mater. Formed as and operating under the Dean, Alumni Affairs and International Relations, the Students' Alumni Cell has been instrumental in organising the Annual Alumni Meet, publishing the quarterly alumni newsletter (KGPian), the annual literary magazine (Yearnings of Yore) and the Annual Yearbook (Colours Of Life) and among other things like organising regular guest lectures by distinguished alumni in the campus. The Students' Alumni Cell also operates the Student - Alumni Mentorship Programme, My Imprint (Giving Back - Alumni contribution) and organises Alvida (the annual farewell dinner). We look forward to taking up more initiatives and organizing more such events to foster the bond between the institute and the alumni.
@@ -203,7 +203,7 @@
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">Mentorship <i class="material-icons right">close</i> <br> Programme</span>
-      <p>The Students’ Alumni Cell will begin the second phase of this year’s Student Alumni Mentorship Programme, one of its most successful initiatives, in October.The SAMP Program saw a participation of more t han 700 student and 300 alumni in its previous phase.</p>
+      <p>The Students&#8217; Alumni Cell will begin the second phase of this year&#8217;s Student Alumni Mentorship Programme, one of its most successful initiatives, in October.The SAMP Program saw a participation of more t han 700 student and 300 alumni in its previous phase.</p>
     </div>
   </div>
 </div>
@@ -218,7 +218,7 @@
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">Guest Lecture<i class="material-icons right">close</i></span>
-      <p>Continuing with its yearlong series of Guest Lectures by eminent alumni, the Students Alumni Cell recently hostel Mr. Veerendra Jaitltey, an alumnus of the ECE department, 1979. He delivered a motivational lecture with the theme ‘We Can, We Can’.</p>
+      <p>Continuing with its yearlong series of Guest Lectures by eminent alumni, the Students Alumni Cell recently hostel Mr. Veerendra Jaitltey, an alumnus of the ECE department, 1979. He delivered a motivational lecture with the theme &#8217;We Can, We Can&#8217;.</p>
     </div>
   </div>
 </div>
@@ -233,7 +233,7 @@
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">Yearbook<i class="material-icons right">close</i></span>
-      <p>The Yearbook is a collection of photographs and memories presented every year at the time of the convocation to the graduating batch by the Students' Alumni Cell. It is the perfect souvenir for everyone about to leave their home away from home, KGP.....  </p>
+      <p>The Yearbook is a collection of photographs and memories presented every year at the time of the convocation to the graduating batch by the Students&#8217; Alumni Cell. It is the perfect souvenir for everyone about to leave their home away from home, KGP.....  </p>
     </div>
   </div>
 </div>
