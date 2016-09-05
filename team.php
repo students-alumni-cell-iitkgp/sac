571534884e30889053a5b820cc7f2685f312905a
arsalansaad <arsalansaad.iitkgp@gmail.com>
2016-09-05 22:21:55 +0530

    team.php changes updated

---
+++ team.php
@@ -0,0 +1,211 @@
+<html>
+<head>
+	<title>The Team | Students' Alumni Cell &middot; IIT Kharagpur</title>
+   <link rel="icon" href="img/meet_14.png">
+	  <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/social_icon.css">
+	  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	  <script src="js/materialize.min.js"></script>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<?php include'navbar.php';?>
+	
+	<div class="container">
+	<div class="row">
+	<div class="heading">
+		<center>
+			<h class="subheading "><b>General Secretaries</b></h>
+		</center>
+	</div>
+	<div class="col l4 offset-l2">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator responsive-img " src="img/team/vaibhav.jpg">
+    </div>
+    <div class="card-content" >
+      <span class="card-title activator grey-text text-darken-4" >Vaibhav Chauhan</span>
+       <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/vaibhavchauhan06"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/vaibhavchauhan6"></a></li></ul>
+    </div>
+   
+  </div>
+	</div>
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/mridul.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Mridul Pant</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/mridul.pant?"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/mridul-pant-752a6537"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	</div>
+  <div class="heading">
+    <center>
+      <h class="subheading"><b>Coordinators</b></h>
+    </center>
+  </div>
+	<div class="row">
+	<div class="col l4">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/afzal.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Afzal Ahmed</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/afzal.ahmed.754"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/afzal-jawed-ahmed-571987103"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/ashay.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Ashay Walke</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/ashay.walke"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/ashaywalke"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	<div class="col l4">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/ikde.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Ashutosh Ikade</span>
+      <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/ashutosh.ikade"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/ashutoshikade"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/harsh.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Harsh Kumar</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/harsh.sharan.7"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/harsh-kumar-sharan-b41986103"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/pallavi.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Pallavi Das</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/das.pallavi"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/pallavi-das-025a10b0"></a></li></ul>
+    </div>
+   
+  </div>
+	</div>
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/shruti.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Shruti Sagar</span>
+        <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/profile.php?id=100004483579064"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/shruti-sagar-720b28103"></a></li></ul>
+    
+    </div>
+   
+  </div>
+	</div>
+  </div>
+  <div class="heading">
+    <center><h class="subheading"><b>Web Heads</b></h></center>
+  </div>
+		<div class="row">
+	<div class="col l4 offset-l2">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/punit.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Punit Kumar</span>
+     <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/punit.kumar.9809672"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/punit121"></a></li></ul>
+    </div>
+    
+  </div>
+	</div>
+	<div class="col l4 ">
+	<div class="card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/team/utsav.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Utsav Agarwal</span>
+       <ul class="soc"><li><a class="soc-facebook" href="https://www.facebook.com/uagarwal744"></a></li>
+       <li><a class="soc-linkedin" href="https://www.linkedin.com/in/utsav-agarwal-b23164b5"></a></li></ul>
+    </div>
+   
+  </div>
+	</div>
+	
+	</div>
+  
+	</div>
+<div class="row">
+    <div class="col s12">
+      <ul class="tabs">
+        <li class="tab col s3"><a href="#2015">2015</a></li>
+        <li class="tab col s3"><a href="#2014">2014</a></li>
+        <li class="tab col s3"><a href="#2013">2013</a></li>
+        <li class="tab col s3"><a href="#2012">2012</a></li>
+      </ul>
+    </div>
+
+     <div id="2015" class="col s12">
+     
+      <div class="row">
+      
+      <ul>
+        <span><b> <li>Pranshu Jain</li>
+        <li>Rohan Gupta</li>
+        <li>Akash Tulsani</li>
+        <li>Ankit Sharma</li>
+        <li>Manognya Deepthi G</li>
+         
+        <li> Soumyadeep Ghosh</li>
+        <li> Sanket Rihen</li> 
+        <li>Chheda Raja Barnwal</li>
+        <li>Siddharth Kannan</li>
+        <li>Ashish Verma</li> </b> </span>
+      </ul>
+     </div>
+     
+      
+     </div>
+
+    <div id="2014" class="col s12">2014</div>
+    <div id="2013" class="col s12">2013</div>
+    <div id="2012" class="col s12">2012</div>
+  </div>
+  
+  <?php include 'footer.php';?>
+</body>
+
+</html>
