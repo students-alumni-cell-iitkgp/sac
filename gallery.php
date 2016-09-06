53790e125a53701c47195ce06aa4a6878f94dd87
Harshit2575 <harshitmishra1331@gmail.com>
2016-09-06 23:54:28 +0530

    modified for small screens

--- gallery.php
+++ gallery.php
@@ -9,55 +9,55 @@
 </head>
 <body> <center>
 <div class="row">
-<div class="col l3 m3 s6">
+<div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/2.jpg" width="200" >
     </div>
-    <div class="col l3 m3 s6">
+    <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/7.JPG" width="200">
 </div>
-<div class="col l3 m3 s6">
+<div class="col l3 m3 s12">
         <img  class="materialboxed" src="img/gallery/9.JPG" width="200" height="140"/>
     </div>
-    <div class="col l3 m3 s6">
+    <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/63.JPG" width="200"/> 
 </div> </div>
 <div class="row">
-<div class="col l3 m3 s6">
+<div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/18.JPG" width="200" height="140"/>
 </div>
-<div class="col l3 m3 s6">
+<div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/43.JPG" width="200"/>
     </div>
-    <div class="col l3 m3 s6">
+    <div class="col l3 m3 s12">
         <img  class="materialboxed" src="img/gallery/31.JPG" width="200"/>
     </div>
-    <div class="col l3 m3 s6">
+    <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/46.JPG" width="200"/>
    </div> </div>
    <div class="row">
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/66.jpg" width="200"/>
    </div>
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/22.jpg" width="200"/>
    </div>
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/15.jpg" width="200"/>
    </div>
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/85.jpg" width="200"/>
    </div> </div>
    <div class="row">
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/44.jpg" width="200"/>
 </div>
-<div class="col l3 m3 s6">
+<div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/35.jpg" width="200"/>
    </div>
-   <div class="col l3 m3 s6">
+   <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/60.jpg" width="200"/>
         </div> 
-        <div class="col l3 m3 s6">
+        <div class="col l3 m3 s12">
         <img class="materialboxed" src="img/gallery/57.jpg" width="200">
         </div> </div>
         </center>
