c75a6661944039b716e719d068a08cd6cc3f92af
Punit kumar <punit.kumar1661@gmail.com>
2016-09-08 00:42:32 +0530

    Update aam.php

--- aam.php
+++ aam.php
@@ -165,7 +165,7 @@ $result = $connection->query($query);
   <div class="col l4 s12 m4">
   <div class="info z-depth-2 aam-reg" class="valign-wrapper">
     <center>
-      <a href="signup.php" target="_blank"><input type="submit" value="Register" class="waves-effect waves-light btn-large" style="width:250px;padding:6px;"></a>
+      <a href="signup.php" class="waves-effect waves-light btn-large modal-trigger" style="width:250px;" target="_blank">Register</a>
     </center>  
 <!-- Modal Trigger -->
     <center>
