c9c2dd14b867505e75277d8e69d044c796adeb0f
karannaoh <karanpratapsingh43@gmail.com>
2016-09-23 19:39:09 +0530

    footer for mobile version

--- footer.php
+++ footer.php
@@ -7,6 +7,11 @@
                 display: none;
             }
         }
+        @media only screen and (max-width: 414px){
+
+        }
+
+
     </style>
     </head>
 
@@ -14,7 +19,7 @@
 <footer class="page-footer black">
 
     <div class="row">
-      <div class="col s4">
+      <div class="col m10 l4">
                 <!--h5 class="white-text">Footer Content</h5-->
                 <p class="grey-text text-lighten-4">Office of Alumni Affairs & International Relations,<br>
                    Indian Institute of Technology, Kharagpur<br>
@@ -22,10 +27,10 @@
                     Ph: 03222-282236 <br>
                     Email: aao@hijli.iitkgp.ernet.in</p>
             </div>
-            <div id="map" class=" col s4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <div id="map" class=" col l4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
 
-        <div class="col s4 ac-soc">
+        <div id="social_icons" class="col l4 m6 ac-soc">
 
             <ul class="soc" style="padding-left: 5em">
                 <li><a class="soc-facebook" href="http://www.facebook.com/iitkgp.alumnicell?fref=ts&ref=br_tf" target="_blank"></a></li>
