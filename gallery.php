8518cbeb287a634819bc3f96ba68a4c689e9f69a
Harshit2575 <harshitmishra1331@gmail.com>
2016-09-26 22:41:20 +0530

    adding gallery.php

---
+++ gallery.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<link rel="icon" href="img/meet_14.png">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <link rel="stylesheet" href="css/materialize.min.css">
+<title>Gallery</title>
+
+</head>
+<body> <center>
+<div class="row">
+<div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/two.jpg" width="200" >
+    </div>
+    <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/three.JPG" width="200">
+</div>
+<div class="col l3 m3 s12">
+        <img  class="materialboxed" src="img/gallery/four.JPG" width="200" height="140"/>
+    </div>
+    <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/five.JPG" width="200"/> 
+</div> </div>
+<div class="row">
+<div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/six.JPG" width="200" height="140"/>
+</div>
+<div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/seven.JPG" width="200"/>
+    </div>
+    <div class="col l3 m3 s12">
+        <img  class="materialboxed" src="img/gallery/eight.JPG" width="200"/>
+    </div>
+    <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/nine.JPG" width="200"/>
+   </div> </div>
+   <div class="row">
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/ten.JPG" width="200"/> 
+    </div>    
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/eleven.JPG" width="200"/>
+   </div>
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/twelve.JPG" width="200"/>
+   </div>
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/thirteen.JPG" width="200"/>
+   </div> </div>
+   <div class="row">
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/forteen.jpg" width="200"/>
+</div>
+<div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/fifteen.JPG" width="200"/>
+   </div>
+   <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/sixteen.JPG" width="200"/>
+        </div> 
+        <div class="col l3 m3 s12">
+        <img class="materialboxed" src="img/gallery/one.JPG" width="200">
+        </div> </div>
+        </center>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.materialboxed').materialbox();
+    });
+</script>
+
+</body>
+</html>   
