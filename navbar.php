2cd8d928449439323c15f19727fb3171130ef0d7
Harshit2575 <harshitmishra1331@gmail.com>
2016-10-05 14:28:07 +0530

    reverting changes

--- navbar.php
+++ navbar.php
@@ -59,7 +59,7 @@ if ($(window).width()>992) {
 
 
   </style>
-  <div class="row" id="logo" style="background-color: white; margin-bottom: 0px; "><div class="col l12 m12 s12 center-align"><img style=" width:139px;" src="img/logo/sac_logo.png"> </div> </div>
+  <div class="row" id="logo" style="background-color: white; margin-bottom: 0px; "><div class="col l12 m12 s12 center-align"><img style=" width:200px;" src="img/logo/sac_logo.png"> </div> </div>
 
 <div  class="white" id="nav" style="margin-top: 0px;">
   <ul id="events" class="dropdown-content">
