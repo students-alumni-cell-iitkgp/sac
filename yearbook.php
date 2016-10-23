15b480448c3b6ce081155b1ae5f714a2e004a21c
Adarsh kashyap <idealadarshkashyap@gmail.com>
2016-10-23 23:09:41 +0530

    Update yearbook.php (#69)

--- yearbook.php
+++ yearbook.php
@@ -18,14 +18,16 @@
       <h class="subheading"><b>Yearbook </b></h>
     </center>
   </div>
- 
+ <div style="padding:3% 8% 6% 8%;">
   <div class="col l12 m12 s12  z-depth-2">
-    <p class="text_align">The yearbook is a hardbound book that attempts to capture memories for the graduating batches of any institute. &#39;Where would we be in thirty years?&#39; &#39;How would I remember my friends with whom I have studied, played, laughed, cried, partied, fought and lived for the past four or five odd years of my life.&#39; These are the questions that bog the minds of the senior year students. This is where the yearbook emerges as the savior. In nostalgic moments of looking back, the yearbook supplements the alumni with visuals to those fleeting memories, photographs of their batch-mates, stills from the events that took place, inspiring words from that professor they looked up to. It gives them an opportunity to relive their golden days.
+    <p class="text_align" style="padding:6% 10% 6% 10%">The yearbook is a hardbound book that attempts to capture memories for the graduating batches of any institute. &#39;Where would we be in thirty years?&#39; &#39;How would I remember my friends with whom I have studied, played, laughed, cried, partied, fought and lived for the past four or five odd years of my life.&#39; These are the questions that bog the minds of the senior year students. This is where the yearbook emerges as the savior. In nostalgic moments of looking back, the yearbook supplements the alumni with visuals to those fleeting memories, photographs of their batch-mates, stills from the events that took place, inspiring words from that professor they looked up to. It gives them an opportunity to relive their golden days.
     The Students&#39; Alumni Cell works throughout the year to produce three separate yearbooks, one for each of the graduating batches of the Undergraduate, Post Graduate and Research Scholar programs. The Director, the Dean of Alumni Affairs and International Relations, are among many professors who wish the graduating batches well for their lives after KGP in their own ways. Sections, dedicated to each of the departments include a message from the Head Of Departments other than mentioning the names and photographs of its graduating students. The yearbooks also showcase all the events that IIT Kharagpur organized in the year that passed. It features what the societies, cells, and organizations have been up to, the same societies that used up most of their evenings at Kharagpur, which as KGpians would agree weren&#39;t always to their liking.
     Each year, the Students&#39; Alumni Cell strives to make the yearbook as wholesome and eliciting of emotion and spirit for the alumni.
     </p>
   </div>
+  </div>
 </div>
+<br><br>
  <div class="container" style="width:100%;">
 	<div class="row" style="margin:auto;width:90%;">
 	<div class="col s6 m6 l3 offset-l1">
