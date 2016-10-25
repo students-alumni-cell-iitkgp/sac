afd292df61d1c4021378179d4c9e88599dc18f3a
yugam1 <yugam66@gmail.com>
2016-10-25 23:06:22 +0530

    team

--- team.php
+++ team.php
@@ -228,7 +228,7 @@
   <div class="heading">
     <center><h class="subheading">Former Team Members</h></center>
   </div>
-<div class="row">
+<div class="row" style="margin-top: 50px;">
     
       <ul style="font-size: 20;"><b>
         <li class="col l2 m2 s2 offset-l1 m1 s1 center-align">2015</li>
