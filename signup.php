99feade119b4750b887d3cb2432a4c744e381099
YUGAM <yugam66@gmail.com>
2017-10-16 22:15:44 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -68,10 +68,6 @@
   $( function() {
     $( ".datepicker" ).datepicker({ dateFormat: 'dd-mm-yy' ,maxDate: '0'});
   } );
-  $(document).ready(function(){
-          // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-          $('.modal-trigger').leanModal();
-        });
     </script>
 </head>
 <body>
