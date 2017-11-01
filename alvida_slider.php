dba1a324a58e8c0fa11ad3384cb89b6aab7955f7
MaxAsif <maxasif@gmail.com>
2017-11-01 09:06:48 +0530

    changes in alvida page

--- alvida_slider.php
+++ alvida_slider.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>Gallery</title>
-<style type="text/css">
+	<style type="text/css">
 	.normal{
 		margin: 10px;
 	}
@@ -9,125 +9,126 @@
 		width: 300px;
 	}
 	@media only screen and (max-width: 1350px) {
-   	.normal{
-    width: 25vw;
-    padding-right: 10px;
-  }
-    .bigg {
-    	width: 48vw !important;
-    	padding-right: 0;
-    }
-}
+		.normal{
+			width: 25vw;
+			padding-right: 10px;
+		}
+		.bigg {
+			width: 48vw !important;
+			padding-right: 0;
+		}
+	}
 	@media only screen and (max-width: 992px) {
-    .normal {
-       height: 200px;
-       width: 45vw;
-    }
-}
+		.normal {
+			height: 200px;
+			width: 45vw;
+		}
+	}
 	@media only screen and (max-width: 600px) {
-    .normal {
-       height: 200px;
-       width: 350px;
-       margin:10px;
-    }
-    .bigg{
-       height: 200px;
-       width: 350px !important;
-       margin:10px;
-       padding-right: 10;
-    }
-}
+		.normal {
+			height: 200px;
+			width: 350px;
+			margin:10px;
+		}
+		.bigg{
+			height: 200px;
+			width: 350px !important;
+			margin:10px;
+			padding-right: 10;
+		}
+	}
 </style>
 
 </head>
 <body> 
 	<div class="heading" style="margin-top:-20px">
-                    <center >
-                        <h  class="subheading"><b id="senior_class_gift">Alvida 2017</b> </h>
-                    </center>
-                </div>
-<center>
-	<div class="row">
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="300px" src="img/Alvida/Alvida_2017/six.JPG"  >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="300px" src="img/Alvida/Alvida_2017/two.JPG"  >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="300px" src="img/Alvida/Alvida_2017/three.JPG"  >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="300px" src="img/Alvida/Alvida_2017/four.JPG"  >
+		<center >
+			<h  class="subheading"><b id="senior_class_gift">Alvida 2017</b> </h>
+		</center>
+	</div>
+	<center>
+		<div class="row">
+
+			<div class="col l6 m6 s12">
+				<img class=" materialboxed normal " src="img/Alvida/Alvida_2017/6.jpg" height="420px">
+			</div>
+			
+			<div class="col l6 m6 s12">
+				<img class=" materialboxed normal " src="img/Alvida/Alvida_2017/2.jpg" height="420px">
+			</div>
+
+			<div class="col l6 m6 s12">
+				<img class=" materialboxed normal " src="img/Alvida/Alvida_2017/8.jpg" height="420">
+			</div>
+			<div class="col l3 m6 s12">
+				<img class=" materialboxed normal" src="img/Alvida/Alvida_2017/4.jpg" height="200">
+			</div>
+
+			<div class="col l3 m6 s12">
+				<img class=" materialboxed normal " src="img/Alvida/Alvida_2017/1.jpg" height="200">
+			</div>
+			<div class="col l3 m6 s12">
+				<img class=" materialboxed normal " src="img/Alvida/Alvida_2017/9.jpg" height="200">
+			</div>
+			<div class="col l3 m6 s12">
+				<img class=" materialboxed normal" src="img/Alvida/Alvida_2017/5.jpg" height="200">
+			</div>
+
 		</div>
 		
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="200px" src="img/Alvida/Alvida_2017/one.JPG"  >
+
+		<div class="heading" style="margin-top:-20px">
+			<center >
+				<h  class="subheading"><b id="senior_class_gift">Alvida 2014</b> </h>
+			</center>
 		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="200px" src="img/Alvida/Alvida_2017/seven.JPG"  >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="200px" src="img/Alvida/Alvida_2017/five.JPG"  >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" height="200px" src="img/Alvida/Alvida_2017/nine.JPG"  >
+		<div class="row">
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal long" src="img/Alvida/three.jpg" height="420" >
+			</div>
+			<div class="col l6 m6 s12">
+				<img class="materialboxed normal bigg" src="img/Alvida/six.jpg" height="420" >
+			</div>
+			<div class="col l3 m6 s12">
+				<img  class="materialboxed normal long" src="img/Alvida/five.jpg" height="420" />
+			</div>
+			<div class="col l6 m6 s12">
+				<img class="materialboxed normal bigg" src="img/Alvida/eight.jpg" height="420" >
+			</div>
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal" src="img/Alvida/seven.jpg" height="200">
+			</div>
+			<div class="col l3 m6 s12">
+				<img  class="materialboxed normal" src="img/Alvida/four.jpg" height="200" />
+			</div>
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal" src="img/Alvida/nine.jpg" height="200" >
+			</div>
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal" src="img/Alvida/ten.jpg" height="200">
+			</div>
+			<div class="col l3 m6 s12">
+				<img  class="materialboxed normal" src="img/Alvida/eleven.jpg" height="200" />
+			</div>
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal" src="img/Alvida/twelve.jpg" height="200" >
+			</div>
+			<div class="col l3 m6 s12">
+				<img class="materialboxed normal" src="img/Alvida/thirteen.jpg" height="200">
+			</div>
+			<div class="col l3 m6 s12">
+				<img  class="materialboxed normal" src="img/Alvida/fourteen.jpg" height="200" />
+			</div>
+
 		</div>
 
-	</div>
+	</center>
 
-	<div class="heading" style="margin-top:-20px">
-                    <center >
-                        <h  class="subheading"><b id="senior_class_gift">Alvida 2014</b> </h>
-                    </center>
-                </div>
-	<div class="row">
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal long" src="img/Alvida/three.jpg" height="420" >
-		</div>
-		<div class="col l6 m6 s12">
-			<img class="materialboxed normal bigg" src="img/Alvida/six.jpg" height="420" wi>
-		</div>
-		<div class="col l3 m6 s12">
-			<img  class="materialboxed normal long" src="img/Alvida/five.jpg" height="420" />
-		</div>
-		<div class="col l6 m6 s12">
-			<img class="materialboxed normal bigg" src="img/Alvida/eight.jpg" height="420" >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal" src="img/Alvida/seven.jpg" height="200">
-		</div>
-		<div class="col l3 m6 s12">
-			<img  class="materialboxed normal" src="img/Alvida/four.jpg" height="200" />
-		</div>
-		   <div class="col l3 m6 s12">
-			   <img class="materialboxed normal" src="img/Alvida/nine.jpg" height="200" >
-		   </div>
-		   <div class="col l3 m6 s12">
-			   <img class="materialboxed normal" src="img/Alvida/ten.jpg" height="200">
-		   </div>
-		   <div class="col l3 m6 s12">
-			   <img  class="materialboxed normal" src="img/Alvida/eleven.jpg" height="200" />
-		   </div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal" src="img/Alvida/twelve.jpg" height="200" >
-		</div>
-		<div class="col l3 m6 s12">
-			<img class="materialboxed normal" src="img/Alvida/thirteen.jpg" height="200">
-		</div>
-		<div class="col l3 m6 s12">
-			<img  class="materialboxed normal" src="img/Alvida/fourteen.jpg" height="200" />
-		</div>
-
-	</div>
-
-</center>
-
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('.materialboxed').materialbox();
-	});
-</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.materialboxed').materialbox();
+		});
+	</script>
 
 </body>
 </html>
