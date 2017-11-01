2d34afc02bd9601d6869b9ab8abd607db46f9aca
Yugam ++ <yugam66@gmail.com>
2017-11-01 18:06:55 +0530

    stationary sponsors added

--- sponsors2017.php
+++ sponsors2017.php
@@ -261,6 +261,16 @@
          <img class="spons" src="img/spons/2017/Stationery Partner/Camlin.jpg" width="200" height="150" />
       </a>
     </div>
+    </div>
+         <div class="row">
+      <div class="col l6 m12 s12" align="center"> <a href="http://www.sfaprint.co.in/" target="_blank">
+         <img class="spons" src="img/spons/2017/Stationery Partner/sfa.png" width="300" height="150" />
+      </a>
+    </div>
+      <div class="col l6 m12 s12" align="center"> <a href="http://www.matrikas.co.in/" target="_blank">
+         <img class="spons" src="img/spons/2017/Stationery Partner/matrikas.jpg" width="250" height="150" />
+      </a>
+    </div>
     </div> <br> <br>
 
   
