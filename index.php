53a5e6e9eb1735e30ca6728a9e0922ec9d0f8549
Karan Pratap Singh <karanpratapsingh43@gmail.com>
2017-11-05 00:09:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="css/materialize.min.css">
      <script src="js/materialize.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/style.css">
   
 
