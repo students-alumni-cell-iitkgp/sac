5004a17d8fb761cc9cad0e0520fb4aab1cc8be58
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2018-01-06 23:16:18 +0530

    spons 2018

---
+++ sponsors2018.php
@@ -0,0 +1,206 @@
+<html>
+<head>
+<title>Sponsors</title>
+<link rel="icon" href="img/meet_14.png">
+<link rel="stylesheet" href="css/materialize.min.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+<style type="text/css">
+  html {
+    font-size: 15px !important;
+  }
+</style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal-trigger').leanModal();
+        });
+    </script>
+
+</head>
+<body>
+<div class="_wrapper" style="overflow: hidden; background-color: white;">
+<?php include 'navbar.php';?>
+    <?php include 'join_us.php' ?>
+
+  <div class="container" align="center">
+    <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Title Sponsor <hr></h2> </div> 
+      <a href="https://www.hdfcbank.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/1.png" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+  <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Associate Title Sponsor <hr></h2> </div> 
+      <a href="http://www.fairandhandsome.net/" target="_blank" >
+          <img class="spons" src="img/spons/2018/2.jpg" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+   <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Co Sponsor <hr></h2> </div> 
+      <a href="https://shyamsteel.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/3.jpg" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+   <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Driven by <hr></h2> </div> 
+      <a href="https://www.bmw.in/" target="_blank" >
+          <img class="spons" src="img/spons/2018/4.png" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+
+   <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Major Sponsor <hr></h2> </div> 
+      <a href="http://www.tatasteel.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/5.jpg" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+   <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Strategic Sponsor <hr></h2> </div> 
+      <a href="https://www.maxlifeinsurance.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/6.jpg" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+
+
+
+ <div class="row"> 
+
+   <div class="col l12 m12 s12"> <h2 class="spons_head" style="text-align:center;" >Event Sponsors <hr></h2> </div>
+   
+     
+   
+    <div class="col l12 m12 s12">
+
+      <div class="col l6 m4 s12" align="center">  <a href="http://bel-india.com/" target="_blank"> <br> <br>
+         <img class="spons"  src="img/spons/2018/7.png" width="190" height="80" />
+      </a> </div>
+
+    <div class="col l6 m4 s12"  align="center">
+       <a href="www.mahanadicoal.in" target="_blank"> <br> <br>
+         <img class="spons"  src="img/spons/2018/8.jpg" width="190" height="80" />
+      </a>
+</div> 
+
+      <div class="col l4 m4 s12" align="center">  <a href="https://www.unionbankonline.co.in/" target="_blank"> <br> <br>
+         <img class="spons"  src="img/spons/2018/9.jpg" width="190" height="80" />
+      </a> </div>
+
+    <div class="col l4 m4 s12"  align="center">
+       <a href="https://www.birlacorporation.com/mp-birla-cement-perfect.html
+" target="_blank"> <br> <br>
+         <img class="spons"  src="img/spons/2018/10.jpg" width="190" height="80" />
+      </a>
+</div> 
+
+    <div class="col l4 m4 s12"  align="center">  
+       <a href="" target="_blank"><br> <br>
+         <img class="spons"  src="img/spons/2018/11.jpg" width="190" height="80" />
+      </a> </div>
+       </div> </div>
+
+      <br><br>
+
+  <div class="row"> 
+
+   <div class="col l12 m12 s12 "> <h2 class="spons_head" style="text-align:center;" >CSR Partners <hr></h2> </div> 
+
+    <div class="col l6 m6 s6 " style="align:center;">
+       <a href="www.edcilindia.co.in/" target="_blank">
+         <img class="spons"  src="img/spons/2018/12.png" width="280" height="200" />
+      </a>
+ </div>
+    <div class="col l6 m6 s6 " style="align:center;">
+       <a href="" target="_blank">
+         <img class="spons"  src="img/spons/2018/13.gif" width="280" height="200" />
+      </a>
+ </div>
+   </div>
+ <br> <br>
+ <div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Hospitality Partner <hr></h2> </div> 
+      <a href="https://www.oyorooms.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/14.png" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+<div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Airline Partner <hr></h2> </div> 
+      <a href="www.airindia.in/" target="_blank" >
+          <img class="spons" src="img/spons/2018/15.png" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+<div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Electronic Partner <hr></h2> </div> 
+      <a href="" target="_blank" >
+          <img class="spons" src="img/spons/2018/16.jpg" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+<div class="row" >
+   <div class="col l12">  <h2 style="text-align:center;">Multiplex Partner <hr></h2> </div> 
+      <a href="https://www.inoxmovies.com/" target="_blank" >
+          <img class="spons" src="img/spons/2018/17.png" width="350" height="170" />
+      </a>
+   </div>  <br> <br>
+
+  <div class="row">
+
+  <div class="col l12 m12 s12"> <h2 style="text-align:center;">Stationary Partner<hr></h2> </div>
+  <div class="col l6 m12 s12" align="center">
+        <a href="" target="_blank">
+          <img class="spons" src="img/spons/2018/18.jpg" width="350px" height="200px"  />
+        </a>
+      </div>
+  <div class="col l6 m12 s12" align="center">
+        <a href="www.roritoworld.com/" target="_blank">
+          <img class="spons" src="img/spons/2018/19.jpeg" width="350px" height="200px"  />
+        </a>
+      </div>
+
+  </div>  <br> <br>
+
+  <div class="row"> 
+  <div class="col l12 m12 s12"> <h2 style="text-align:center;">Photobooth Partner<hr></h2> </div>
+
+     <div class=" col l12 m12 s12" align="center"><a href="https://www.oorjainteractive.com/
+" target="_blank" >
+        <img class="spons" src="img/spons/2018/20.png" width="290" height="140" >
+      </a>
+      </div>
+
+
+
+  </div> <br> <br>
+    <div class="row"> 
+
+   <div class="col l12 m12 s12 "> <h2 class="spons_head" style="text-align:center;" >Apparel Partners <hr></h2> </div> 
+
+    <div class="col l6 m6 s6 " style="align:center;">
+       <a href="" target="_blank">
+         <img class="spons"  src="img/spons/2018/21.jpg" width="280" height="200" />
+      </a>
+ </div>
+    <div class="col l6 m6 s6 " style="align:center;">
+       <a href="https://rightgifting.com/" target="_blank">
+         <img class="spons"  src="img/spons/2018/22.png" width="280" height="200" />
+      </a>
+ </div>
+   </div>
+ <br> <br>
+
+
+  </div>
+      </div>
+  <?php include 'footer.php';?>
+ 
+  </body>
+  </html>
