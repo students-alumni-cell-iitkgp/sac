45637e86eeefe738c3c2a43e4a4e2d78895cb07d
ashk77 <ashutoshsonu99@gmail.com>
2018-10-23 18:49:41 +0530

    index.php: scrolling speed of notice board reduced; sam.php: added register button

--- sam.php
+++ sam.php
@@ -1,70 +1,72 @@
 <html>
-<head>
-  <title>Student Alumni Meet &middot; IIT Kharagpur</title>
-  <link rel="icon"  href="img/meet_14.png">
-  <link rel="stylesheet" href="css/materialize.min.css">
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-  <script src="js/materialize.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <style type="text/css">
- 
-   @media only screen and (min-width: 768px){
-      .ac-sam-cov img{ width: 100%;
-
-        margin-left: 80px;
-    }
-   } 
-    @media only screen and (max-width: 468px){
-      .ac-sam img{
-height: 25%;
-margin: 0px auto;
-max-width:100%;
-    }
-   } 
-    </style>
-  <script>
-   $(document).ready(function(){
-    $('.slider').slider({full_width: true});
-  });
- </script>
-</head>
-<body>
-  <div class="_wrapper" style="overflow: hidden;" >
-    <?php include 'navbar.php';?>
-
-    <div class="row ac-sam">
-        <img src="img/sam3.jpg" style="width: 100%">
-    </div>
-   
-    
-      <div class="row" style="padding:0% 8% 6% 8%;">
-
-          <div class="center-align" >
-              <div class="col l12 z-depth-1">
-                  <div class="heading ac-head" style="border:3px black solid; display: inline-block; padding-top: 2px; margin: 3px;padding-right: 20px;padding-left: 20px;" >
-       
-                          <h4><b>Student Alumni Meet</b></h4> </div>
-                                        
-                  
-                       <p class="summit text_align" style="padding:1% 10% 6% 10%;">
-                          IIT Kharagpur, a premier institute of the nation has a vast alumni network spread over various locations across the world. With an aim to bring together the students and the alumni, not just in the 
-                          premises of the institute, but also regionally, Student Alumni Meets have been initiated. In collaboration with the regional chapters, Student Alumni Meets bring together the students and the alumni 
-                          under one roof and encourage interactive dialogue. They play a very important role in strengthening the bond with alumni.The aim of these meets is to connect the alumni and the 
-                          students at a very personal level and ensure long term interaction and strong bonding thus proving to be extremely beneficial to both the students as well as the alumni. Many such Student Alumni Meets 
-                          have been conducted till now in various cities across India. Last year, Bangalore and Delhi witnessed huge participation.
-                          <br><br>
-                          Furthermore, Student Alumni Meets have facilitated the implementation of various initiatives of 
-                          SAC like Mentorship, Guest Lectures and many others. Further, a platform for the introduction as well as the implementation of many more future initiatives is provided. Most of all, SAMs connect the alumni and the students at a very personal level and ensure long term interaction and strong bonding thus proving to be extremely beneficial to the students as well as the alumni. Student Alumni Meets will continue to play a very important role in the functioning of Students' Alumni Cell.
-                      </p>
-                  </div>
-              </div>
-          </div>
-
-  <?php include 'gallery.php';?>
-</div>
-  
-  <?php include 'footer.php';?>
-</body>
+    <head>
+        <title>Student Alumni Meet &middot; IIT Kharagpur</title>
+        <link rel="icon"  href="img/meet_14.png">
+        <link rel="stylesheet" href="css/materialize.min.css">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        <script src="js/materialize.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <style type="text/css">
+            @media only screen and (min-width: 768px){
+            .ac-sam-cov img{ width: 100%;
+            margin-left: 80px;
+            }
+            } 
+            @media only screen and (max-width: 468px){
+            .ac-sam img{
+            height: 25%;
+            margin: 0px auto;
+            max-width:100%;
+            }
+            } 
+        </style>
+        <script>
+            $(document).ready(function(){
+             $('.slider').slider({full_width: true});
+            });
+        </script>
+    </head>
+    <body>
+        <div class="_wrapper" style="overflow: hidden;" >
+            <?php include 'navbar.php';?>
+            <div class="row ac-sam">
+                <img src="img/sam3.jpg" style="width: 100%">
+            </div>
+            <div class="row" style="padding:0% 8% 6% 8%;">
+                <div class="center-align" >
+                    <div class="col l12 z-depth-1">
+                        <div class="heading ac-head" style=" display: inline-block; padding-top: 2px; margin: 3px;padding-right: 20px;padding-left: 20px;" >
+                            <p><b>Register for Students' Alumni Meet - Kolkata '18   &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;</b>
+                                <a href="samReg.php" class="btn waves-effect waves-light" type="submit" style="background-color:#449d44;" name="button1">Register<i class="material-icons right">send</i>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding:0% 8% 6% 8%;">
+                <div class="center-align" >
+                    <div class="col l12 z-depth-1">
+                        <div class="heading ac-head" style="border:3px black solid; display: inline-block; padding-top: 2px; margin: 3px;padding-right: 20px;padding-left: 20px;margin-top: 30px;" >
+                            <h4><b>Student Alumni Meet</b></h4>
+                        </div>
+                        <p class="summit text_align" style="padding:1% 10% 6% 10%;">
+                            IIT Kharagpur, a premier institute of the nation has a vast alumni network spread over various locations across the world. With an aim to bring together the students and the alumni, not just in the 
+                            premises of the institute, but also regionally, Student Alumni Meets have been initiated. In collaboration with the regional chapters, Student Alumni Meets bring together the students and the alumni 
+                            under one roof and encourage interactive dialogue. They play a very important role in strengthening the bond with alumni.The aim of these meets is to connect the alumni and the 
+                            students at a very personal level and ensure long term interaction and strong bonding thus proving to be extremely beneficial to both the students as well as the alumni. Many such Student Alumni Meets 
+                            have been conducted till now in various cities across India. Last year, Bangalore and Delhi witnessed huge participation.
+                            <br><br>
+                            Furthermore, Student Alumni Meets have facilitated the implementation of various initiatives of 
+                            SAC like Mentorship, Guest Lectures and many others. Further, a platform for the introduction as well as the implementation of many more future initiatives is provided. Most of all, SAMs connect the alumni and the students at a very personal level and ensure long term interaction and strong bonding thus proving to be extremely beneficial to the students as well as the alumni. Student Alumni Meets will continue to play a very important role in the functioning of Students' Alumni Cell.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php include 'gallery.php';?>
+        </div>
+        <?php include 'footer.php';?>
+    </body>
 </html>
