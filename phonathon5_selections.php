a45b750a461b1bdfb879327ec879b1897e26d920
kunal1244 <kunaldas1244@gmail.com>
2019-03-09 16:22:22 +0530

    Added phonathon selections

---
+++ phonathon5_selections.php
@@ -0,0 +1,125 @@
+<html>
+    <head>
+        <title>Phonathon 5.0 Selections</title>
+        <link rel="icon" href="img/meet_14.png">
+        <link rel="stylesheet" href="css/materialize.min.css">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php include 'navbar.php';?>          
+        <style type="text/css">
+            body {
+            }
+            #intro{
+            line-height: 40px;
+            }
+            .card-panel{
+            width: 100% !important;
+            }
+            .headl{
+            background-color: #666;
+            color: white;
+            border-radius: 5px;
+            border-color: black;
+            }
+            .input{
+            background-color: silver;
+            text-align: center;
+            border-radius: 5px;
+            border-color: black;
+            display: inline-block;
+            }
+            .sidenav {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 55;
+            top: 0;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            overflow-y: scroll;
+            transition: 0.5s;
+            padding-top: 60px;
+            text-align:center;
+            }
+            .sidenav a {
+            padding: 8px 8px 8px 32px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+            transition: 0.3s
+            }
+            .sidenav a:hover{
+            color: #f1f1f1;
+            }
+            .sidenav .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+            }
+            #ajax{
+            width: 60% !important;
+            background-color: #fff;
+            margin-left: 20%;
+            padding: 50px;
+            margin-bottom: 50px;
+            z-index: 50000;
+            }
+            @media screen and (max-height: 450px) {
+            .sidenav {padding-top: 15px;}
+            .sidenav a {font-size: 18px;}
+            #ajax{
+            width: 90%;
+            margin:5%;
+            }
+            }
+            @media screen and (min-height: 992px) {
+            #button{
+            display: none;
+            }
+            }
+        </style>
+        <body>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeueFYaiCs_F2qZB9l6d-SR3LRinqLqEThE22LQyNQnGa1n7Q/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            <div class="_footer">
+            <footer class="page-footer black">
+                <div class="row">
+                    <div class="col m10 l4">                        
+                        <p class="grey-text text-lighten-4 text_align" id="address" style="padding-left: 1em">Office of Alumni Affairs & International Relations,<br>
+                            Indian Institute of Technology, Kharagpur<br>
+                            Pin-721302, West Bengal, India<br>
+                            Ph: 03222-282236 <br>
+                            Email: aao@hijli.iitkgp.ernet.in
+                        </p>
+                    </div>
+                    <div id="map" class=" col l4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <div id="social_icons" class="col l4 m6 ac-soc">
+                        <ul id="social_icon" class="soc">
+                            <li style="color: white;" id="connect-template">Connect with us on</li>
+                            <br>
+                            <li><a class="soc-facebook" href="http://www.facebook.com/iitkgp.alumnicell?fref=ts&ref=br_tf" target="_blank"></a></li>
+                            <li><a class="soc-twitter"  href="http://twitter.com/IITKgpAlumni" target="_blank"></a></li>
+                            <li><a class="soc-linkedin"  href="https://www.linkedin.com/company-beta/13273135/" target="_blank"></a></li>
+                            <li><a class="soc-youtube soc-icon" href="https://www.youtube.com/user/alumnicelliitkgp/videos" target="_blank"></a></li>
+                            <li ><a class="socicon-instagram" href="https://www.instagram.com/sac.iitkgp/" target="_blank"  style="font-size: 30px; background-color: red; border-radius: 2000px; "></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-copyright grey darken-3">
+                    <div class="container">
+                        &copy; 2018 Students' Alumni Cell
+                    </div>
+            </footer>
+            </div>
+            <script type="text/javascript" src="js/style.js"></script>
+    </body>
+</html>
