c8e7fa74aee58936cbcccd4dccdc44ac062031f8
Kunal <kunaldas1244@gmail.com>
2019-10-15 03:03:40 +0530

    Added SAM Regs

--- sam_reg.php
+++ sam_reg.php
@@ -91,7 +91,7 @@
   <body>
 
   
-  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehKTeYS45tIXw77srtcbucbvux3QXL3eXH3ZsvNj9KEvm4Vw/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe9h6YfnMe-FKTV25oE0sJIUHwPK23izg240aXDjODyPMWV6w/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     
     
        
