ba9c80aa19f8dc7b6e670e9e9ea6992f20d4b099
divyanshgupta56 <devdivy56@gmail.com>
2020-07-15 21:41:49 +0530

    Affinity page updated

--- affinity.php
+++ affinity.php
@@ -165,7 +165,7 @@
   </div>
 
             <div class="row">
-                  <div class="col l4 s1 m1">
+                  <div class="col l2 s1 m2">
                     -
                   </div>
                       <div class="col l4 s12 m4">
@@ -197,6 +197,26 @@
                              </p>
                             </div>
                         </div>
+                      </div>
+                        <div class="col l4 s12 m4">
+                        
+                        <div class="card z-depth-0 _card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" style="" src="img/afinity/geek.png">
+                            </div>
+                            
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
+                                <p class="text_align" style="font: 20px"><b>Avail the special offers now.</b><br>
+                                1.50% Discount on Amazon SDE Test series <br>
+
+                                2. 25% Discount on Data Structures & Algorithm course<br>
+                             <a href=https://lm.facebook.com/l.php?u=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fcourses%2Fonline%3Ffbclid%3DIwAR1-zfhfePh_nbITuRw83AUrbhqUIAhxGG9dcqnACpcPd0yaI2yjpqhhumc&h=AT2_tlm-K4l51Fw7EpLlMArnoapR3WVYkpbrtNcp0u5cBv1ZxayQBK_eclqGcEI6X-KSRPJiBirv6iqno5b3OD78VmhqzvoZ7Abb3f21d6pc8diH8Qy2t0J6aZupLTp4CZfLlnVsuPoy42qknI4YcARAtbSJjGw7I11dQw>CLICK HERE</a> 
+                             </p>
+                            </div>
+                        </div>
+                    </div>
+                  
                     </div>
                     <!-- <div class="col l3" style="margin-top: 0.2em;height:450px">
                             <div id="fbplug" style="background-color: #fff" class="z-depth-1 fb-page" data-href="https://www.facebook.com/iitkgp.alumnicell/?fref=ts" data-tabs="timeline" data-height="485px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" style="height: 450px">
@@ -290,268 +310,4 @@
 
   </body>
   </html>
-=======
-<html>
-     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.marquee/1.3.1/jquery.marquee.min.js" charset="utf-8"></script>
-<head>
-<title>Affinity Programme</title>
- <link rel="icon" href="img/meet_14.png">
-<link rel="stylesheet" href="css/materialize.min.css">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="js/materialize.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
 
-
-<style type="text/css">
-    @media only screen and (max-width: 769px){
-        .class_gift_Img
-        {
-            padding-left: 5%;
-        }
-      .ac-imprint img
-      {
-        margin: 0px auto;
-        width:100%;
-        height: 35%;
-      }
-      .subheading
-      {
-        font-size: 5.5vw;
-      }
-      p{
-       font-size: 3.3vw; 
-      }
-
-    }
-    @media only screen and (max-width: 415px)
-    {
-      .ac-imprint img{
-        margin: 0px auto;
-        width:100%;
-        height: 25%;
-      }
-      .class_gift_Img
-      {
-          padding-left: 2%;
-          width:100%;
-          height:auto;  
-      
-      }
-
-      .subheading
-      {
-        font-size: 5.5vw;
-      }
-      p{
-       font-size: 4.8vw; 
-      }
-      #button  h5 
-      {
-        font-size: 6vw;
-        line-height: 121%;
-      }
-    }
-   @media only screen and (min-width: 768px)
-   {
-
-    .imprint-top {
-      margin-top: 20px;
-      margin-left: 60px;
-      width: 860px;
-      height: 60px;
-      border-radius: 6px;
-      margin-bottom: 20px;
-    }
-      .ac-imprint img
-      {
-        margin: 0px auto;
-        width: 100%;
-      }
-    #imgleft
-    {
-      margin-left: 8%;
-    }
-    #cg-17
-    {
-      margin-left: -11%;
-
-    }
-    
-
-
-
-          .text
-           {
-            color: black;
-            font-size: 30px;
-            position:;
-            top: 80%;
-            left: 70%;
-            -webkit-transform: translate(0%, 10%);
-            -ms-transform: translate(0%, 10%);
-            transform: translate(0%, 10%);
-            text-align:;
-            opacity: 1;
-          }
-    }
-
-
-    @media only screen and (max-width: 468px){
-      .ac-imprint img{
-        margin: 0px auto;
-        max-width:100%;
-    }
-    .imprint-top {
-      max-width: 100%;
-      height: 60px;
-    }
-
-   }
-</style>
-    <script>
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-    </script>
-</head>
-<body>
-<div class="_wrapper">
-<?php include 'navbar.php';?>
-
- <div class="row ac-imprint">
-        <img src="img/afinity/wall.jpg"  >
-    </div>
-
-  <div class="row">
-    <div class="col l12 s12 m12">
-        <div class="info z-depth-1">
-            <div class="heading" style="margin-top: -10px;">
-                  <center >
-                      <h  class="subheading"><b id="h1" >Affinity Programme</b> </h>
-                  </center>
-             </div>
-                   <p style="padding:15px; font-size:16px;">Students' Alumni Cell is a pioneer in volunteer-based organisations, since it's inception it has strived towards building connections between the students and the alumni community. One of the only initiative of its kind in the institute is the IIT Kharagpur Affinity Programme, an initiative that is undertaken by the SAC to benefit the entire KGP community ie. alumni and students alike. We strive towards establishing strong and firm relations with multinational companies by establishing two-way ties in which they provide KGPians with incentives and unimaginable discounts on goods and services and in return these companies benefit by gaining a huge client base an unprecedented marketing. Thus, this initiative is an excellent way to help instigate a feeling of privilege and pride to be part of the IIT Kharagpur community. <br><br><b> Alumni can register for Institute Email Address <a href="http://www.alumni.iitkgp.ac.in/AlumniIITKGP/EmailReg?fbclid=IwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w" target="_blank"> Here.</a></b></p>
-
-              </div>
-         </div>
-      </div>
-
-  <div class="row" style="padding-top:10px;">
-    <center>
-      <a id="button" style="height:8%;;padding-top:0px;color:black;" class="waves-effect waves-light btn">  <h5><b> Current Association</b> </h5></a>
-    </center>
-  </div>
-
-            <div class="row">
-                  <div class="col l4 s1 m1">
-                    -
-                  </div>
-                      <div class="col l4 s12 m4">
-                        
-                        <div class="card z-depth-0 _card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="img/afinity/samsung.jpg">
-                            </div>
-                            
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
-                                <p class="text_align" style="font: 20px"><b>The benefits of this association include:</b><br>
-                            1. Special Prices<br>
-                            2. Affordable EMIs<br>
-                            3. Free 3rd party accidental and liquid damage insurance<br>
-                            4. Easy Exchange<br>
-                            5. <a href='https://www.samsung.com/in/store/campus/?fbclid=IwAR1rAE87hHn5Y3_GSEesTQCQmmT791oXJL8H1yHklGMl6C-qTCFZ1kVhsno' target='_blank'>CLICK HERE</a> 
-                             </p>
-                            </div>
-                        </div>
-                    </div>
-                  
-  </div>
-  </div>
-
-
-
-
-
-
-<div style='background-color:#F4F4F4'>
-  <div class="row" style="padding-top:10px;">
-    <center>
-      <a id="button" style="height:8%;;padding-top:0px;color:black;" class="waves-effect waves-light btn">  <h5><b> Previous Associations</b> </h5></a>
-    </center>
-  </div>
-
-  <div class="row">
-          <div class="col l2 "> 
-            -
-          </div>
-         
-                  <div class="col l4 s12 m4">
-                    
-                        <div class="card z-depth-0 _card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="img/afinity/Oyo.jpg" style= " width: 95.5%">
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
-                                <p class="text_align"><b>Oyo offering the following stay options: </b><br>
-                                    1) Short term stays/Group Tours/Events: Located near Corporate clusters, Uninterrupted Connection, Basic Amenities, Dedicated Security Personnel.<br>
-                                    2) Long term stays: Fully Furnished, Fully Managed, Long Term Rental, Zero Brokerage, One month Security Only. <br>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col l4 s12 m4">
-                    
-                        <div class="card z-depth-0 _card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="img/afinity/hdfc.png">
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
-                                <p class="text_align"><b>HDFC offering the following options: </b><br>
-                                    The benefits of this association includes:
-                                      New upgraded HDFC bank Diners Club Black Credit Card. (Annual Memberships, Monthly Milestone benefits and other added features).<br>
-                                     
-                                       
-                                      <a href="Diners-Privilege.pdf">Life Time Free Diners Black Card</a> <br>
-                                      <a href="Diners-Black-New.pdf">Priced Diners Black card</a><br>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-  
-    
-
-     </div>
-  <?php include 'footer.php';?>
-   <script type="text/javascript">
-            $(".card").mouseenter(function(e){
-              if ($(this).find('> .card-reveal').length) {
-                if ($(e.target).is($('.card .activator')) || $(e.target).is($('.card .activator i')) ) {
-                  // Make Reveal animate up
-                  $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
-                }
-              }
-              $('.card-reveal').closest('.card').css('overflow', 'hidden');
-            });
-            $(".card").mouseleave(function(){
-              // Make Reveal animate down and display none
-              $(this).find('.card-reveal').velocity(
-                {translateY: 0},
-                {
-                  duration: 225,
-                  queue: false,
-                  easing: 'easeInOutQuad',
-                  complete: function() {
-                    $(this).css({ display: 'none'});
-                  }
-                });
-              });
-        </script>
-
-  </body>
-  </html>
