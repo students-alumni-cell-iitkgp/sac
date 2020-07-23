bd9fed27b81eafdac0cd7948d842d1acd8e57445
divyanshgupta56 <devdivy56@gmail.com>
2020-07-23 14:14:51 +0530

    affinity page updated

--- affinity.php
+++ affinity.php
@@ -142,7 +142,7 @@
 
  <div class="row ac-imprint">
         <img src="img/afinity/wall.jpg"  >
-    </div>
+ </div>
 
   <div class="row">
     <div class="col l12 s12 m12">
@@ -154,9 +154,9 @@
              </div>
                    <p style="padding:15px; font-size:16px;">Students' Alumni Cell is a pioneer in volunteer-based organisations, since it's inception it has strived towards building connections between the students and the alumni community. One of the only initiative of its kind in the institute is the IIT Kharagpur Affinity Programme, an initiative that is undertaken by the SAC to benefit the entire KGP community ie. alumni and students alike. We strive towards establishing strong and firm relations with multinational companies by establishing two-way ties in which they provide KGPians with incentives and unimaginable discounts on goods and services and in return these companies benefit by gaining a huge client base an unprecedented marketing. Thus, this initiative is an excellent way to help instigate a feeling of privilege and pride to be part of the IIT Kharagpur community. </p>
 
-              </div>
-         </div>
-      </div>
+        </div>
+    </div>
+  </div>
 
 
 
@@ -169,118 +169,76 @@
   </div>
 
             <div class="row">
-                  <div class="col l2 s1 m2">
-                    -
-                  </div>
-                      <div class="col l4 s12 m4">
-                      <!-- <div class="card">
-                            <img src="img_avatar.png" alt="Avatar" style="width:100%">
-                            <div class="container">
-                              <h4><b>John Doe</b></h4> 
-                              <p>Architect & Engineer</p> 
-                            </div>
-                          </div> -->
-                        
+                      <div class="col l2 s1 m2">
+                        -
+                      </div>
+                      <div class="col l4 s12 m4">  
                         <div class="card z-depth-0 _card">
                             <div class="card-image waves-effect waves-block waves-light">
                                 <img class="activator" style="" src="img/afinity/samsung.jpg">
-                                
-                                      <img class="activator" src="Galaxy_buds.jpg">
-                                     <!-- <p>Architect & Engineer</p>  -->
-                                
+                                <img class="activator" src="Galaxy_buds.jpg">
+                            </div>
+                            <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
+                                  <p class="text_align" style="font: 20px"><b>The benefits of this association include:</b><br>
+                              1. Special Prices<br>
+                              2. Affordable EMIs<br>
+                              3. Free 3rd party accidental and liquid damage insurance<br>
+                              4. Easy Exchange<br>
+                              5. <a href='https://www.samsung.com/in/store/campus/?fbclid=IwAR1rAE87hHn5Y3_GSEesTQCQmmT791oXJL8H1yHklGMl6C-qTCFZ1kVhsno' target='_blank'>CLICK HERE</a> 
+                               </p>
+                              </div>  
+                        </div>
+                      </div>
+                      <div class="col l4 s12 m4">
+                        
+                        <div class="card z-depth-0 _card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" style="" src="img/afinity/geek.png">
+                            </div>
+                            
+                            <div class="card-reveal">
+                                    <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
+                                    <p class="text_align"style="font: 15px"><b>Avail the special offers now.</b><br>
+                                    1).50% Discount on Amazon SDE Test series <br>
+
+                                    2). 25% Discount on Data Structures & Algorithm course<br>
+                                    3). Use coupon code KGPIAN_PERKS (For Amazon SDE) & KGPIAN_PERKS_DSA (For DSA Course)<br>
+                                 <a href=https://lm.facebook.com/l.php?u=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fcourses%2Fonline%3Ffbclid%3DIwAR1-zfhfePh_nbITuRw83AUrbhqUIAhxGG9dcqnACpcPd0yaI2yjpqhhumc&h=AT2_tlm-K4l51Fw7EpLlMArnoapR3WVYkpbrtNcp0u5cBv1ZxayQBK_eclqGcEI6X-KSRPJiBirv6iqno5b3OD78VmhqzvoZ7Abb3f21d6pc8diH8Qy2t0J6aZupLTp4CZfLlnVsuPoy42qknI4YcARAtbSJjGw7I11dQw>CLICK HERE</a> 
+                                </p>
                             </div>
                         </div>
                       </div>
-                        <div class="col l4 s12 m4">
+            </div>
+            <div class="row">
+                      <div class="col l4 s4 m4">
+                        -
+                      </div>
+                      <div class="col l4 s12 m4">
+                        <div class="card z-depth-0 _card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/afinity/a_v2.jpg">
+                            </div>
                             
                             <div class="card-reveal">
                                 <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
-
                                 <p class="text_align" style="font: 20px"><b>The benefits of this association include:</b><br>
                             1. Courses with discount coupons.<br>
                             2. A chance to get internship for selected enrolled students.<br>
                             3. Discounted passes for Annual data Science conference for selected students.<br>
-                            4. <a href='https://www.samsung.com/in/store/campus/' target='_blank'>CLICK HERE</a> 
-
-                                <p class="text_align"style="font: 15px"><b>Avail the special offers now.</b><br>
-                                1).50% Discount on Amazon SDE Test series <br>
-
-                                2). 25% Discount on Data Structures & Algorithm course<br>
-                                3). Use coupon code KGPIAN_PERKS (For Amazon SDE) & KGPIAN_PERKS_DSA (For DSA Course)<br>
-                             <a href=https://lm.facebook.com/l.php?u=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fcourses%2Fonline%3Ffbclid%3DIwAR1-zfhfePh_nbITuRw83AUrbhqUIAhxGG9dcqnACpcPd0yaI2yjpqhhumc&h=AT2_tlm-K4l51Fw7EpLlMArnoapR3WVYkpbrtNcp0u5cBv1ZxayQBK_eclqGcEI6X-KSRPJiBirv6iqno5b3OD78VmhqzvoZ7Abb3f21d6pc8diH8Qy2t0J6aZupLTp4CZfLlnVsuPoy42qknI4YcARAtbSJjGw7I11dQw>CLICK HERE</a> 
-
+                            4. <a href='http://www.analyticsvidhya.com/' target='_blank'>CLICK HERE</a> 
                              </p>
                             </div>
                         </div>
-                    </div>
-                    <!-- <div class="col l3" style="margin-top: 0.2em;height:450px">
-                            <div id="fbplug" style="background-color: #fff" class="z-depth-1 fb-page" data-href="https://www.facebook.com/iitkgp.alumnicell/?fref=ts" data-tabs="timeline" data-height="485px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" style="height: 450px">
-                                <blockquote cite="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ" class="fb-xfbml-parse-ignore"><a href="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ">Alumni Email Registration</a></blockquote>
-                            </div>
-                        </div> -->
-                  
-                    </div>
-                    <!-- <div class="col l3" style="margin-top: 0.2em;height:450px">
-                            <div id="fbplug" style="background-color: #fff" class="z-depth-1 fb-page" data-href="https://www.facebook.com/iitkgp.alumnicell/?fref=ts" data-tabs="timeline" data-height="485px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" style="height: 450px">
-                                <blockquote cite="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ" class="fb-xfbml-parse-ignore"><a href="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ">Alumni Email Registration</a></blockquote>
-                            </div>
-                        </div> -->
-                  
+                      </div>
+           </div>
   </div>
-  </div>
+
+
 
 
 
 <div style='background-color:#F4F4F4;'>
-
-            <div class="row">
-                                    <div class="col l4 s1 m1">
-                    -
-                  </div>
-                      <div class="col l4 s12 m4">
-                      <!-- <div class="card">
-                            <img src="img_avatar.png" alt="Avatar" style="width:100%">
-                            <div class="container">
-                              <h4><b>John Doe</b></h4> 
-                              <p>Architect & Engineer</p> 
-                            </div>
-                          </div> -->
-                        
-                        <div class="card z-depth-0 _card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="img/afinity/a_v2.jpg">
-                              
-                                
-                            </div>
-                            
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4">-<i class="material-icons right"></i></span>
-                                <p class="text_align" style="font: 20px"><b>The benefits of this association include:</b><br>
-                            1. Courses with discount coupons.<br>
-                            2. A chance to get internship for selected enrolled students.<br>
-                            3. Discounted passes for Annual data Science conference for selected students.<br>
-                            4. <a href='https://www.samsung.com/in/store/campus/?fbclid=IwAR1rAE87hHn5Y3_GSEesTQCQmmT791oXJL8H1yHklGMl6C-qTCFZ1kVhsno' target='_blank'>CLICK HERE</a> 
-                             </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- <div class="col l3" style="margin-top: 0.2em;height:450px">
-                            <div id="fbplug" style="background-color: #fff" class="z-depth-1 fb-page" data-href="https://www.facebook.com/iitkgp.alumnicell/?fref=ts" data-tabs="timeline" data-height="485px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" style="height: 450px">
-                                <blockquote cite="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ" class="fb-xfbml-parse-ignore"><a href="https://www.alumni.iitkgp.ac.in%2FAlumniIITKGP%2FEmailReg%3Ffbclid%3DIwAR2WiaCRHFqOZEaDaPfVrIognLqdz9qJ6nZiCVYtrpt3HlD3jbZbAFLjy3w&h=AT0tUwBONlf2P_seQDk2-jheJxVbufizpIMqYl1sNRbvRk_q2SZbtGur2Yi-y5gz5QAYh-wtEkMGrvGdX95IPh2QBGu-IrH4rgNboZSDl-Gk-jjh07J5Wjca2xAMTgxVSiOniQ">Alumni Email Registration</a></blockquote>
-                            </div>
-                        </div> -->
-                  
-  </div>
-  </div>
-
-
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 78d7d2f40b4d2eb4e9a6ec0186d92d540ff68565
-<div style='background-color:white;'>
   <div class="row" style="padding-top:10px;">
     <center>
       <a id="button" style="height:8%;;padding-top:0px;color:black;" class="waves-effect waves-light btn">  <h5><b> Previous Associations</b> </h5></a>
@@ -288,7 +246,7 @@
   </div>
 
   <div class="row">
-          <div class="col l2 "> 
+          <div class="col l2 m2"> 
             -
           </div>
          
