c8bc4f9a2f632de3c1561408f0ad9291cf5af98f
AryanLohani <64068225+AryanLohani@users.noreply.github.com>
2021-03-27 15:09:45 +0530

    changed name in teams.php

--- homecoming.php
+++ homecoming.php
@@ -8,7 +8,7 @@ $(function () {
 
           $.ajax({
             type: 'post',
-            url: 'aamlogin.php',
+            url: 'aamlogin.php',  
             data: $('#form1').serialize(),
             success: function (response) {
                if(response== 0 )
@@ -219,17 +219,14 @@ else
   <center>
     <div class="heading ac-head" style="border:3px black solid;display:inline-block;padding-top:2px;margin:3px;padding-right:20px;padding-left:20px;">
         <h4 style="margin-bottom: 0px;" ><b>Homecoming 2020</b> </h4> <br>
-
         <h5  style="margin-top: 0px;">Postponed on account of COVID-19</h5>
         
       </div>
       <div class="heading ac-head" style="display:inline-block;padding-top:2px;margin:3px;padding-right:20px;padding-left:20px;">
       <p style="width:90%;padding:10px;font-size:17px;text-align:justify">
           It is with a heavy heart that we have to inform you that the 3rd edition of the Homecoming, which was originally scheduled for August 2020 has been postponed due to the Corona Virus Pandemic. We wish everyone good health in these difficult times and will pray for the safety and well being of each of you. We will convey any further developments regarding the Homecoming in the future.
-       </p>
+      </p>
         
-=======
-        <h5  style="margin-top: 0px;">POSTPONED ON ACCOUNT OF COVID-19</h5>
       </div>
     </center>
   <div class="col l12 s12 m12">
@@ -241,14 +238,15 @@ else
   </center>
   </div>
   
+  </div>
 <!-- <div class="row" >
 
-          <!-- <div class="offset-l3 col l3"><a class="waves-effect waves-light btn-large modal-trigger" href="#modal1" style="width: 105%;height:5em;padding-top: 10px" >LOGIN</a>
+          <div class="offset-l3 col l3"><a class="waves-effect waves-light btn-large modal-trigger" href="#modal1" style="width: 105%;height:5em;padding-top: 10px" >LOGIN</a>
           </div>
               <br><br>
           <div class="col l3"><a href="signup.php" class="waves-effect waves-light btn" id="sweetalert" style="width: 105%;height: 5em">Homecoming 2020 <br> Registration</a>
           </div>
-           -->
+          
     
 </div> -->
 <!--
