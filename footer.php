504b03e54f74e6b2378fce60d6ce0c16a6ee442d
tanwir1703 <64023039+tanwir1703@users.noreply.github.com>
2021-07-20 17:12:54 +0530

    Home Page Updates
    Updates in the About Us, Footer and Notice Board

--- footer.php
+++ footer.php
@@ -43,11 +43,11 @@
     <div class="row">
       <div class="col m10 l4">
                 <!--h5 class="white-text">Footer Content</h5-->
-                <p class="grey-text text-lighten-4 text_align" id="address" style="padding-left: 1em">Office of Alumni Affairs & International Relations,<br>
+                <p class="grey-text text-lighten-4 text_align" id="address" style="padding-left: 1em">Office of Alumni Affairs & Branding,<br>
                    Indian Institute of Technology, Kharagpur<br>
                     Pin-721302, West Bengal, India<br>
                     Ph: 03222-281860 <br>
-                    Email: aao@hijli.iitkgp.ernet.in</p>
+                    Email: aao@hijli.iitkgp.ac.in</p>
             </div>
             <!-- <div id="map" class=" col l4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div> -->
