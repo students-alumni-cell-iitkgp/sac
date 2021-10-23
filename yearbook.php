9477c1c09af09db2bbf21f882e00634e37d56b2e
tanwir1703 <tanwir1703@gmail.com>
2021-10-24 00:39:23 +0530

    AAM bug fixes

--- yearbook.php
+++ yearbook.php
@@ -32,13 +32,6 @@
             color: #00416d !important;
             border-color: #00416d !important;
         }
-        body{
-            background: #ff512f;
-            /* fallback for old browsers */
-            background: url(../img/yearbook/main-building-with-stars.jpg);
-            background-size: cover;
-            background-position: bottom;
-        }
 
     </style>
 </head>
