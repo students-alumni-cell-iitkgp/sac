dc575a5601e7eb34e6a368031f13fccdfe515433
anjaniit23 <anjaniit23@gmail.com>
2021-10-23 13:15:54 +0530

    added css in alumnitalks,alvida,aam,index,navbar

--- footer.php
+++ footer.php
@@ -1,74 +1,149 @@
- 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
 <head>
-    <link rel="stylesheet" href="css/social_icon.css">
+    <meta charset="utf-8">
+    <title>Students' Alumni Cell, IIT Kharagpur</title>
+
+    <!-- Bootstrap Scripts CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Bootstrap scripts Jquery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
     <style>
-         #social_icon{
-            padding-left: 5em;
-            margin:20px;
-        }
-        @media only screen and (max-width: 992px){
-            #map{
-                display: none;
-            }
-            #address{
-                font-size:3.7vw;
-            }
-            #social_icons{
-                font-size:3.7vw;
-            }
-            .soc li a{
-                width: 10vw;
-                height: 10vw;
-                line-height: 10vw;
-                font-size: 6vw;
-            }
-            #social_icon{
-            padding-left: -0.2em;
-            }
-            #connect-template{
-                padding-top: 5vw;
-                padding-left: 15vw;
-            }
-            #heading{
-                font-family: cursive;
-                color:white
-            }
-        }
-       
+    * {
+        margin: 0;
+        padding: 0;
+
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        width: 100%;
+    }
+
+    #footer {
+        margin-top:30px;
+        background-color: #23232b;
+        height: 19rem; 
+    }
+
+/* 
+    .footer-top-left {
+        height: 12rem;
+        background-image: linear-gradient(to bottom right,pink,pink);
+        color: white;
+        padding-top: 1.8rem;
+    } */
+
+    .footer-top-right {
+        padding: 1.8rem;
+        color: #eef0ef;
+
+    }
+
+    /* .dots {
+        font-size: 2em;
+        position: relative;
+        bottom: 2.5rem;
+    } */
+
+    .bar {
+        font-size: 3em;
+        color: white;
+        position: absolute;
+        top: -2.8rem;
+        left: 2rem;
+
+
+    }
+	@media(max-width:768px){
+		.icon{
+	font-size:1em;
+	padding:0;}}
+	
+
+    .icon {
+        font-size: 2em;
+        color: white;
+        position: absolute;
+        top: -1rem;
+
+    }
+
+    #copyright {
+        background-color: black;
+        text-align: center;
+        color: grey;
+    }
     </style>
-      <link href="https://file.myfontastic.com/n6vo44Re5QaWo8oCKShBs7/icons.css" rel="stylesheet"> 
-    </head>
-<div class="_footer">
-<footer class="page-footer black">
-    <div class="row">
-      <div class="col m10 l4">
-                <!--h5 class="white-text">Footer Content</h5-->
-                <p class="grey-text text-lighten-4 text_align" id="address" style="padding-left: 1em">Office of Alumni Affairs & Branding,<br>
-                   Indian Institute of Technology, Kharagpur<br>
-                    Pin-721302, West Bengal, India<br>
-                    Ph: 03222-281860 <br>
-                    Email: aao@hijli.iitkgp.ac.in</p>
+
+</head>
+
+<body>
+
+
+    <section id='footer'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-4 ' style=" padding-top: 1.8rem;   color: white;  height: 12rem;">
+                    <h2> Get Social <br>With Us</h2>
+                </div>
+                <div class="col-2"></div>
+                <div class='col-6 footer-top-right'>
+                    <h5>Students' Alumni Cell, IIT Kharagpur</h5>
+                    <p>Office of Alumni Affairs & Branding,<br>
+                        Indian Institute of Technology, Kharagpur<br>
+                        Pin-721302, West Bengal, India<br><br>
+                        <em>Ph: 03222-281860</em> <br>
+                        <em>Email: aao@hijli.iitkgp.ac.in</em>
+                    <hr>
+                </div>
             </div>
-            <!-- <div id="map" class=" col l4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div> -->
-        <div class="col m10 l4">
-          <p id="heading" color="white">Get social with us</p>
+            <div class='row'>
+                <div class="col-2">
+                    <a href="https://www.facebook.com/iitkgp.alumnicell"  target="_blank"><i class="icon fab fa-facebook"></i></a>
+                </div>
+                <div class='col-2'>
+                    <a href="https://www.instagram.com/sac.iitkgp/?hl=en" target="_blank"><i class="icon fab fa-instagram"></i></a>
+                </div>
+                <div class='col-2'>
+                    <a href="https://twitter.com/IITKgpAlumni" target="_blank"><i class="icon fab fa-twitter-square"></i></a>
+                </div>
+                <div class='col-2'>
+                    <a href="https://studentsalumnicell-iitkgp.medium.com/" target="_blank"><i class="icon fab fa-medium"></i></a>
+                </div>
+                <div class='col-2'>
+                    <a href="https://www.linkedin.com/company/sac-iitkgp/mycompany/" target="_blank"><i class="icon fab fa-linkedin"></i></a>
+                </div>
+				<div class='col-2'>
+                    <a href="https://www.youtube.com/c/StudentsAlumniCellIITKharagpur" target="_blank"><i class="icon fab fa-youtube"></i></a>
+                </div>
+            </div>
         </div>
-        <div id="social_icons" class="col l4 m6 ac-soc">
-            <ul id="social_icon" class="soc">
-               <li style="color: white;" id="connect-template">Get social with us</li> <br>
-                <li><a class="soc-facebook" href="http://www.facebook.com/iitkgp.alumnicell?fref=ts&ref=br_tf" target="_blank"></a></li>
-                <li><a class="soc-twitter"  href="http://twitter.com/IITKgpAlumni" target="_blank"></a></li>
-                <li><a class="soc-linkedin"  href="https://www.linkedin.com/company-beta/13273135/" target="_blank"></a></li>
-                <li><a class="soc-youtube soc-icon" href="https://www.youtube.com/user/alumnicelliitkgp/videos" target="_blank"></a></li>
-                <li ><a class="socicon-instagram" href="https://www.instagram.com/sac.iitkgp/" target="_blank"  style="font-size: 30px; background-color: red; border-radius: 2000px; "></a></li>
-            </ul>
-        </div>
-    </div>
-  <div class="footer-copyright grey darken-3">
-    <div class="container">
-    &copy; 2018 Students' Alumni Cell
-    </div>
-</footer>
-</div>
-<script type="text/javascript" src="js/style.js"></script>
+        <!-- 	Footer end	 -->
+  	</section>
+    <!--    Copyright -->
+  	<section id='copyright'>
+  		<p>&copy; 2020 Students' Alumni Cell</p>
+  	</section>
+
+
+  </body>
+</html>
