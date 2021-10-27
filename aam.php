4413f5e258b332ffd4bbb5d752df61eb4de99791
anjaniit23 <anjaniit23@gmail.com>
2021-10-27 23:10:17 +0530

    com

--- aam.php
+++ aam.php
@@ -498,7 +498,7 @@
   </section>
 
 	<?php include'footer.php' ?>
-	<!-- <?php include'preloader.php' ?> -->
+	<!-- <?//php include'preloader.php' ?> -->
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
