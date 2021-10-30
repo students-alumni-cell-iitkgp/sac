6fbeca5446f523c82a52a4e11ce75b07071566cb
shubhangsingh6023 <SHUBHANGSINGH2002@GMAIL.COM>
2021-10-30 20:10:47 +0530

    compressed

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="/form.css">
     <!-- JS, Popper.js, and jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
@@ -115,6 +115,7 @@
                 </div>
             </div>
 
+            
             <div class="section3">
                 <div class="heading">
                     <h2>Accomodation</h2>
@@ -122,159 +123,7 @@
                         <div id="one" class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height:0.4rem;"></div>
                     </div>
                 </div>
-                <!--jhjyh-->
-                <!-- <h1 class="text-center" >Registration Fees</h1> -->
-                <table class="mb-5 border-light" style="margin-bottom: 2vh !important;" >
-                    <th colspan="4" class="text-center h3 text-decoration-underline font-weight-bold"> Fixed charges</th>
-                    <tr>
-                       <td data-th="">Alumnus/Alumna </td>
-                       <td data-th="No.of Rooms"></td>
-                       <td style="text-align: center;" data-th="Cost for Alumni">7500</td>
-                       <td style="text-align: center;" data-th="Cost for Accompaniaments"></td>
-                    </tr>
-                    <tr>
-                       <td data-th="">Accompaniments </td>
-                       <td data-th="No.of Rooms"></td>
-                       <td style="text-align: center;" data-th="Cost for Alumni">4500</td>
-                       <td style="text-align: center;" data-th="Cost for Accompaniaments"></td>
-                    </tr>
-                </table>
-                <table class="mb-3 border-light" >
-                    <!-- <caption>Room Charges</caption> -->
-                    <tr><th colspan="4" class="text-center h3 text-decoration-underline font-weight-bold"> Accomodation charges</th></tr>
-                    <tr>
-                       <th> Guest House</th>
-                       <!-- <th>No.of Rooms</th> -->
-                       <th style="text-align: center;">Cost per person(₹)</th>
-                       <!-- <th style="text-align: center;">Cost for Accompaniaments(₹)</th> -->
-                    </tr>
-                    <!-- <tr>
-                       <td data-th="">Technology Guest House - SO</td>
-                       
-                       <td style="text-align: center;" data-th="Cost for Alumni">3000</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">Technology Guest House - DO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">2250</td>
-                       </tr> -->
-                    <tr>
-                        <td data-th="">Technology Guest House/ Alumni Guest House</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">2250</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">VGH/ SAM</td>
-                        <!-- <td data-th="No.of Rooms">7</td> -->
-                        <td style="text-align: center;" data-th="Cost for Alumni">900</td>
-                        <!-- <td style="text-align: center;" data-th="Cost for Accompaniaments"></td> -->
-                    </tr>
-                    <!-- <tr>
-                        <td data-th="">VGH/CEC - AC - DO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">900</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">VGH/CEC - Non-AC - SO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">750</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">VGH 3/4  bedded- DO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">450</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">AGH - DO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">2250</td>
-                       </tr>
-                    <tr>
-                        <td data-th="">SAM - DO</td>
-                        <td style="text-align: center;" data-th="Cost for Alumni">900</td>
-                       </tr> -->
-                </table>
-
-                <!-- <table class="mb-5 border-light" style="margin-bottom: 2vh !important;" >
-                <th colspan="4" class="text-center h3 text-decoration-underline font-weight-bold"> Accompaniment's charge</th>
-                    <tr>
-                        <td data-th="">Accomodation per person </td>
-        
-                        <td style="text-align: center;" data-th="Cost for Alumni">2250</td>
-                        <td style="text-align: center;" data-th="Cost for Accompaniaments"></td>
-                    </tr> 
-                </table> -->
-                <!-- <p style="margin-left: 15px; margin-bottom:0 !important; text-align:center;"><i>*Current or past work experiences as per relevance</i></p> -->
-                <div class="container">
-                    <div class="row">
-                       <div class="col-sm-12 col-md-6">
-                           <label for="marital">Marital Status</label>
-                           <input class="form-control" type="text" name="marital" id="marital">
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <label for="accompanyingNo" >Accompaniments (Number of guests)<span style="color:red;">*</span></label>
-                            <input class="form-control" type="number" min="0" id="accompanyingNo" name="accompanyingNo" onchange="calc_cost()" required>
-                        </div>
-                        <div class="col-sm-12">
-                            <label for="room">Guest House <span style="color:red;">*</span></label>
-                            <div class="input-group mb-3">
-                              
-                                <select  class="form-control form-select" type="list" onchange="calc_cost()"  list="room" id="room" name="room" required>
-                                <!-- <datalist id="room"> -->
-                                <option value=""></option> 
-                                     <option value="Technology Guest House/ Alumni Guest House">Technology Guest House/ Alumni Guest House</option> 
-                                    <!-- <option value="Technology Guest House - DO | Acc">Technology Guest House - DO | Acc(Double Occupancy | Accomodation)</option> -->
-                                    <!-- <option value="Technology Guest House - DO | Acc(Double Occupancy | Accomodation)"> -->
-                                    <!-- <option value="Technology Guest House - DO | Al(Double Occupancy | Shared)">Technology Guest House - DO | Al(Double Occupancy | Shared)</option> -->
-                                     <option value="VGH/ SAM">VGH/ SAM</option>
-                                    <!-- <option value="VGH/CEC - AC - DO | Al"> VGH/CEC - AC - DO | Al </option> 
-                                    <option value="VGH/CEC - Non-AC - SO"> VGH/CEC - Non-AC - SO</option> 
-                                    <option value="VGH 3/4  bedded- DO | Al"> VGH 3/4  bedded- DO | Al</option> 
-                                    <option value="AGH - DO | Acc"> AGH - DO | Acc</option> 
-                                    <option value="SAM - DO | Al "> SAM - DO | Al </option>  -->
-                                </select>
-                            </div >
-    
-                            <!-- </datalist> -->
-                            <!--<h4 id="cost" name = "cost" type="text" style="text-align: center;">Total cost = &#8377;0</h4>-->
-                            <input class="form-control" id="cost" name = "cost" type="text" value = "Total cost = &#8377; 0 " readonly>
-                        </div>
-                        <!-- <div class="col-sm-12 col-md-6">
-                            <label for="desig">Designation <span style="color:red;">*</span></label>
-                            <input class="form-control" type="text" id="desig" name="designation" required>
-                        </div>
-                        <div class="col-sm-12">
-                            <label for="address">Address</label>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text" style="font-weight: 600;" id="basic-addon1"><i class="fas fa-address-card"></i></span>
-                                <input class="form-control" type="text" id="address" name="work_address">
-                            </div>
-                            
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <label for="city">City</label>
-                            <input class="form-control" type="text" id="city" name="work_city">
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <label for="state">State</label>
-                            <input class="form-control" type="text" id="state" name="work_state">
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <label for="Country">Country</label>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text" style="font-weight: 600;" id="basic-addon1"><i class="fas fa-globe"></i></i></span>
-                                <input class="form-control" type="text" id="Country" name="work_country">
-                            </div>
-                            
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <label for="zip">Zip Code</label>
-                            <input class="form-control" type="text" id="zip" name="work_zipCode">
-                        </div> -->
-                    </div>
-                </div>
-                <div class="row justify-content-md-between justify-content-around">
-                    <div class=" col-md-2 col-3">  <button class="btn btn-dark"  type = "button" onclick="back1()">Back</button> </div>
-                    <div class=" col-md-2 col-3">     <button class="btn btn-dark" type = "button" onclick="next2()">Next</button></div>
-                </div>
-                
-                <!-- <button class="btn btn-dark" onclick="next2()">Next</button> -->
             </div>
-            
             <div class="section4">
                 <div class="heading">
                     <h2>Work Experience</h2>
@@ -596,7 +445,7 @@
         //        cost = 900 + 7500;
         //    }
 
-           document.getElementById("cost").value =  "Total Cost = Rs"+cost;
+           document.getElementById("cost").value =  "Total Cost = Rs "+cost;
 
            console.log(cost);
        }
