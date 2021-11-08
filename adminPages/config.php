d6a7c4a655be95ae4d340c416511eafeeb3ebd1f
AryanLohani <64068225+AryanLohani@users.noreply.github.com>
2021-11-09 00:12:05 +0530

    server commit , adding connection

--- adminPages/config.php
+++ adminPages/config.php
@@ -1,12 +1,10 @@
  <?php 
-  $host="localhost";
+  include '../connection.php';
   $db="aam";
-  $user="root";
-  $pass="";
   $charset="utf8mb4";
-  $dsn="mysql:host=$host;dbname=$db;charset=$charset";
+  $dsn="mysql:host=$DB_HOST;dbname=$db;charset=$charset";
   try { 
-    $pdo= new PDO($dsn,$user,$pass);
+    $pdo= new PDO($dsn,$DB_USER,$DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e ) {
       echo "<h1>Error connecting to db</h1>";
