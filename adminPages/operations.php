d3a2ea46540e80e30584a0615e5ad5636772158e
anjaniit23 <anjaniit23@gmail.com>
2021-11-12 01:58:38 +0530

    added the displayRegisterUsers.php page

--- adminPages/operations.php
+++ adminPages/operations.php
@@ -1,10 +1,10 @@
 <?php 
 
-function insert($name,$email,$accompaniments,$payment,$vaccination,$gh){
+function insert($name,$email,$accompaniments,$payment,$vaccination,$gh,$yog){
       try{ 
             //defining the sql statement to be executed
             //backticks are used instead of single quotes!!
-            $sql="INSERT INTO `aam`( `name`, `email`,`accompaniments`, `reciept`, `status`,`gh`) VALUES (:name,:email,:accompaniments,:payment,:vaccination,:gh)";
+            $sql="INSERT INTO `aam`( `name`, `email`,`accompaniments`, `reciept`, `status`,`gh`,`yog`) VALUES (:name,:email,:accompaniments,:payment,:vaccination,:gh,:yog)";
             //preparing the sql statement for execution
             $stmt=$GLOBALS["pdo"]->prepare($sql);
             $stmt->bindparam(':name',$name);
@@ -13,6 +13,7 @@ function insert($name,$email,$accompaniments,$payment,$vaccination,$gh){
             $stmt->bindparam(':payment',$payment);
             $stmt->bindparam(':vaccination',$vaccination);
             $stmt->bindparam(':gh',$gh);
+            $stmt->bindparam(':yog',$yog);
             $stmt->execute();
             return true;
 
@@ -69,15 +70,33 @@ function viewrecord($id){
   //       return false;
   //   }
   // }
-  function delete($id){
+  function change_boolean($email,$boolean){
     try {
-      $sql="DELETE FROM `aam` WHERE `id`=:id";
-      $stmt=$GLOBALS['pdo']->prepare($sql);
-      $stmt->bindparam(':id',$id);
-      $stmt->execute();
+      $bool=($boolean=='1')?'0':'1';
+      $sql2="UPDATE `aam` SET `boolean` = :bool WHERE `email` = :email;";
+          //preparing the sql2 statement for execution
+      $stmt2=$GLOBALS["pdo"]->prepare($sql2);
+      $stmt2->bindparam(':bool',$bool);
+      $stmt2->bindparam(':email',$email);
+      $result=$stmt2->execute();
       return true;
   
     } catch(PDOException $e){
+      echo $e->getmessage();
+      return false;
+    }
+  }
+
+  function getBatch($batch){
+    try {
+      $sql="SELECT * FROM `aam`  WHERE `yog`=:batch AND `boolean`='1' ";
+          //preparing the sql2 statement for execution
+      $stmt=$GLOBALS["pdo"]->prepare($sql);
+      $stmt->bindparam(':batch',$batch);
+      $stmt->execute();
+      return $stmt;
+  
+    }catch(PDOException $e){
       echo $e->getmessage();
       return false;
     }
