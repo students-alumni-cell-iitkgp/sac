68e0495b6c886df6d9f7d6668febc36deeed69e6
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2022-03-05 23:20:01 +0530

    Changed Dean in About us

--- index.php
+++ index.php
@@ -143,7 +143,7 @@
                             </div>
                             <p class="pt-4" style="opacity: 1" align="justify">
                                 The Students' Alumni Cell, IIT Kharagpur is a voluntary student body working under the
-                                aegis of the Dean of Alumni Affairs and International Relations. It strives to bridge
+                                aegis of the Dean, Outreach. It strives to bridge
                                 the gap between IIT Kharagpur and its esteemed alumni community.
 
                                 The Students' Alumni Cell continuously endeavours to strengthen the relations of the
