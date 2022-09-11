063d0ece7d581544122f4fcc0da8efc3fc0b658b
puravi-238 <kajolsahu238@gmail.com>
2022-09-11 13:34:53 +0530

    backend upload photo

--- backend/signup.php
+++ backend/signup.php
@@ -120,31 +120,32 @@ include '../config.php';
         </button>
       </div>'; 
 
-        // file name, type, size, temporary name 
-        $file_name = $_FILES['pro_pic']['name']; 
-        $file_type = $_FILES['pro_pic']['type']; 
-        $ext=explode('.',  $file_name);
-        $file_tmp_name = $_FILES['pro_pic']['tmp_name']; 
-        $file_size = $_FILES['pro_pic']['size']; 
+      $em=explode('@',  $email);
+      // file name, type, size, temporary name 
+      $file_name = $_FILES['pro_pic']['name']; 
+      $file_type = $_FILES['pro_pic']['type']; 
+      $ext=explode('.',  $file_name);
+      $file_tmp_name = $_FILES['pro_pic']['tmp_name']; 
+      $file_size = $_FILES['pro_pic']['size']; 
 
+   
+      // target directory 
+      $target_dir = "../uploads/"; 
+      $fetch_dir="./uploads/";
      
-        // target directory 
-        $target_dir = "../uploads/"; 
-        $fetch_dir="./uploads/";
-       
-        // uploding file 
-        if(move_uploaded_file($file_tmp_name,$target_dir.$mobile.'.'.$ext[1])) 
-        { 
+      // uploding file 
+      if(move_uploaded_file($file_tmp_name,$target_dir.$em[0].'.'.$ext[1])) 
+      { 
 
-          $q = "update `hc` SET `pro_pic` = '$fetch_dir$mobile.$ext[1].' WHERE `email` = '$email'"; 
-          $stmt=$GLOBALS["conn"]->prepare($q);
-          $result=$stmt->execute();
-          
-        } 
-        else 
-        { 
-          echo "File can not be uploaded"; 
-        } 
+        $q = "update `hc` SET `pro_pic` = '$fetch_dir$em[0].$ext[1].' WHERE `email` = '$email'"; 
+        $stmt=$GLOBALS["conn"]->prepare($q);
+        $result=$stmt->execute();
+        
+      } 
+      else 
+      { 
+        echo "File can not be uploaded"; 
+      } 
 
         if($gh == 'TGH')
         {
