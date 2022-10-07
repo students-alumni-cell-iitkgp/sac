02a18d37ac0c3832209e328c1da8ce549288e37e
Shashank3kgp <shashanktupakulaiitkgp2021@gmail.com>
2022-10-07 13:56:22 +0530

    changed the name of the homecoming file

--- navbar.php
+++ navbar.php
@@ -30,7 +30,7 @@
 						     	<li><a class="dropdown-item" href="alumni_talks.php"> Alumni Talks </a></li>
 								 <li><a class="dropdown-item" href="alvida.php"> Alvida </a></li>
 								 <li><a class="dropdown-item" href="aam.php">Annual Alumni Meet </a></li>
-								 <li><a class="dropdown-item" href="homecomingnew.php"> Homecoming </a></li>
+								 <li><a class="dropdown-item" href="homecoming2022.php"> Homecoming </a></li>
 								 <li><a class="dropdown-item" href="leadershipsummit.php"> Leadership Summit </a></li>
 								 <li><a class="dropdown-item" href="phonathon.php"> Phonathon </a></li>
 								<li><a class="dropdown-item" href="sam.php"> Students' Alumni Meet </a></li>			
