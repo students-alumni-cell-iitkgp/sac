f6d908b4169127008f8ec46c9ef78a99e26601d8
anjaniit23 <anjaniit23@gmail.com>
2022-11-01 23:18:39 +0530

    updated paystatus

--- backend/reciept.php
+++ backend/reciept.php
@@ -7,7 +7,7 @@
 
         $email = $_SESSION['email'];
         
-        $sql = "UPDATE `hc` SET `reciept` = '$reciept' AND 'pay_status'=1 WHERE `email` = '$email'";
+        $sql = "UPDATE `hc` SET `reciept` = '$reciept' , `pay_status` = 1 WHERE `email` = '$email'";
         $stmt=$GLOBALS["conn"]->prepare($sql);
         $result = $stmt->execute();
         if($result){
