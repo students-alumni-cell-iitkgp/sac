afe8a915378a3993b5beaa7fba59e02b2d12b675
pushpu77 <pushpukumar77@gmail.com>
2023-02-04 08:03:14 +0530

    sponsor page added

---
+++ sponsors2023.php
@@ -0,0 +1,318 @@
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Students' Alumni Cell | Sponsors</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--FONT AWESOME--><link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	        <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js"></script>
+        <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ScrollTrigger.min.js"></script>
+  <link rel="stylesheet" href="css/sponsors.css">
+</head>
+
+<body>
+ <?php include 'navbar.php'; ?>
+    <?php include 'register_spons.php'; ?>
+    <br><br><br>
+        <div class="_wrapper" style="overflow: hidden; background-color: white;">
+            <div class="container" align="center">
+                <!-- new -->
+                <div class="s-1 row" >
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                            Title Sponsor
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12">
+                      <a href="https://www.hdfcbank.com/" target="_blank" >
+                      <img class="spons" src="img/spons/2023/hdfcTitle.webp" width="300" height="300" />
+                      </a>
+                    </div>
+                </div>
+                <br> <br>
+                <!-- new -->
+                <div class="s-2 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Associate Title Sponsor
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12"align="center">
+                        <a href="https://www.jsw.in/" target="_blank">
+                        <img class="spons" src="img/spons/2023/associateJSW.png" width="300" height="230px"  />
+                        </a>
+                    </div>
+                </div>
+                <br><br>
+                <!-- new -->
+                <div class="s-3 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Strategic Partner
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12" align="center">
+                        <a href="https://iocl.com/" target="_blank">
+                        <img class="spons" src="img/spons/2023/strategicIndian.webp" style="position: relative; top:25px;" width="220px" height="200px"  />
+                        </a>
+                    </div>
+                </div>
+                <br><br><br>
+                <!-- new -->
+                <div class="s-9 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                            Major Sponsor
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12" align="center">
+                        <a href="https://sunsource-energy.com/" target="_blank">
+                        <img class="spons" src="img/spons/2023/sunSourceMajorSponsor.webp" width="300px" height="220px"  />
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12" align="center">
+                        <a href="https://www.uneecops.com/" target="_blank">
+                        <img class="spons" src="img/spons/2023/uneecops.png" width="300px" height="220px"  />
+                        </a>
+                    </div>
+                </div>
+
+                <br>
+                <br>
+
+               <!-- new new -->
+
+               <div class="s-11 row">
+                    <div class="col-sm-12">
+                        <h2 class="spons_head" style="text-align:center;" >
+                            Event Partner
+                            <hr>
+                        </h2>
+                    </div>
+                  </div>
+                    <div class="s-11 row">
+                        <div class="col-lg-4 col-md-6 col-sm-12"  align="center">
+                            <a href="https://www.hindustanpetroleum.com/" target="_blank"><br> <br>
+                            <img class="spons"  src="img/spons/2023/hpEvent.webp" width="200" height="170" />
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12" align="center">
+                            <a href="https://bizacuity.com/" target="_blank">
+                            <br><br>
+                            <img class="spons"  src="img/spons/2019/es1.webp" width="190" height="150" />
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12" align="center">
+                            <a href="https://www.grinfra.com/" target="_blank">
+                            <br><br>
+                            <img class="spons"  src="img/spons/2023/gril.png" width="190" height="150" />
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-12"  align="center">
+                            <a href="https://www.acilnet.com/" target="_blank"> <br> <br>
+                            <img class="spons"  src="img/spons/2023/AhluwaliaEvent.webp" width="190" height="200" />
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-md-12 col-sm-12"  align="center">
+                            <a href="https://www.lbicgroup.com/" target="_blank"> <br> <br>
+                            <img class="spons"  src="img/spons/2023/LBICEvent.webp" width="190" height="200" />
+                            </a>
+                        </div>
+
+                    
+                    </div>
+                    <br>
+                    <br>
+
+
+                <!-- new -->
+                <div class="s-5 row" >
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Exclusive Media Partner
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12" align="center">
+                        <a href="https://inshorts.com/en/read/" target="_blank">
+                        <img class="spons" src="img/spons/2023/inshortsEvent.webp" width="280px" height="200px"  />
+                        </a>
+                    </div>
+                </div>
+                <br><br>
+                <div class="s-6 row">
+                        <div class="col-sm-12">
+                            <h2 style="text-align:center;">
+                                Exclusive Gifting partner
+                                <hr>
+                            </h2>
+                            <br><br>
+                        </div>
+                        <br><br>
+                        <div class="col-sm-12" align="center">
+                            <a href="https://www.borosil.com/" target="_blank">
+                            <img class="spons" src="img/spons/2023/borosilExclusiveGifting.webp" width="320px" height="120px"  />
+                            </a>
+                        </div>
+                    </div>
+                    <br><br>
+                    
+                    <div class="s-7 row">
+                        <div class="col-sm-12">
+                            <h2 style="text-align:center;">
+                            Beverage Partner
+                                <hr>
+                            </h2>
+                            <br>
+                        </div>
+                        <div class="col-sm-12" align="center">
+                            <a href="https://www.amazon.in/GrainDelites-Junk-Chocolate-Health-Drink/dp/B081V17F7B/ref=pd_lpo_1?pd_rd_w=XxEoM&content-id=amzn1.sym.6fa6e9ce-2890-49c2-bd25-c1096b5f4cf4&pf_rd_p=6fa6e9ce-2890-49c2-bd25-c1096b5f4cf4&pf_rd_r=1SW6AAQ4SB3387R8T9RX&pd_rd_wg=Pp5sQ&pd_rd_r=2f86e4b4-837a-411c-bba0-3d53306b437c&pd_rd_i=B081V17F7B&psc=1" target="_blank">
+                            <img class="spons" src="img/spons/2023/greatBanyanBeverage.webp" width="280px" height="120px"  />
+                            
+                            </a>
+                        </div>
+                    </div>
+                   
+                    
+                <br><br>
+                <!-- new -->
+                <div class="s-9 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        <br>
+                        Corporate Gifting Partner
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12" align="center">
+                        <a href="https://deinkleider.com/" target="_blank">
+                        <img class="spons" src="img/spons/2023/DeinKleiderCorporateGifting.webp" width="270px" height="210px"  />
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12" align="center">
+                        <a href="https://tirupatibagcentre.com/" target="_blank">
+                        <img class="spons" src="img/spons/2023/tirupatiCorporate.webp" width="270px" height="210px"  />
+                        </a>
+                    </div>
+                </div>
+                
+
+                <br><br>
+                <div class="s-12 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Radio Partner
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12" align="center">
+                        <a href="https://www.excellentpublicity.com/radio/friends-fm-919-kolkata" target="_blank">
+                        <img class="spons" src="img/spons/2023/FriendsRadio.webp" width="300px" height="200px"  />
+                        </a>
+                    </div>
+                </div>
+                <br>
+                 <div class="s-13 row">
+                        <div class="col-sm-12">
+                            <h2 style="text-align:center;">
+                            Stationary Partner
+                                <hr>
+                            </h2>
+                            <br><br>
+                        </div>
+                        <div class="col-sm-12" align="center">
+                            <a href="https://www.reynolds-pens.com/" target="_blank">
+                            <img class="spons" src="img/spons/2023/reynoldsStationary.webp" width="280px" height="120px"  />
+                            </a>
+                        </div>
+                    </div>
+                    <br><br>
+
+                <!-- new -->
+                <div class="s-14 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                            Online Media Partner
+                            <hr>
+                        </h2>
+                    </div>
+                  </div>
+                    <div class="s-14 row">
+                        <div class="col-lg-4 col-md-6 col-sm-12" align="center">  <a href="https://www.campuskarma.in/" target="_blank"> <br> <br>
+                            <img class="spons"  src="img/spons/2023/CampusKarmaOnlineMediaPartner.webp" width="190" height="180" />
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12"   align="center">
+                            <a href="https://www.jiffindia.org/" target="_blank"><br> <br>
+                            <img class="spons"  src="img/spons/2023/JIFFOnlineMedia.webp" width="300" height="150" />
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-sm-12"  align="center">
+                            <a href="https://www.fistosports.com/" target="_blank"> <br> <br>
+                            <img class="spons"  src="img/spons/2023/FistoSportsOnlineMedia.webp" width="190" height="160" /><br>
+                            </a>
+                        </div>
+                    </div>
+                <br><br>
+                
+
+                <div class="s-17 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Saving(s) partner
+                        
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12" align="center">
+                        <a href="https://www.grabon.in/" target="_blank">
+                        <br>
+                        <img class="spons" src="img/spons/2023/GrabOnSavings.webp" width="330px" height="200px"  />
+                        </a>
+                    </div>
+                </div>
+    
+                <br><br>
+            
+                <br>
+                <!-- new -->
+                <div class="s-20 row">
+                    <div class="col-sm-12">
+                        <h2 style="text-align:center;">
+                        Booking Partner
+                            <hr>
+                        </h2>
+                    </div>
+                    <div class="col-sm-12" align="center">
+                        <a href="https://www.easemytrip.com/?utm_source=google&utm_medium=cpc&utm_campaign=788997081&utm_content=39319940377&utm_term=easemytrip&utm_campaign=788997081&utm_source=g_c&utm_medium=cpc&utm_term=e_easemytrip&adgroupid=39319940377&gclid=CjwKCAiA_vKeBhAdEiwAFb_nrXua8YQNZJUzveY4zQH43MmSxHI_b32d55D5SVngqknpnBRPL6oKgxoCYUIQAvD_BwE" target="_blank">
+                        <img class="spons" src="img/spons/2023/EaseMyTripBooking.webp" width="270" height="170px"  />
+                        </a>
+                    </div>
+                </div>
+                <br><br>
+                
+            </div>
+        </div>
+        <?php include 'footer.php';?>
+        <!-- <?php include 'preloader.php' ?> -->
+        <script type="text/javascript">
+        for (var i = 1; i <31; i++) {
+          gsap.from(".s-"+i+" h2",{scrollTrigger:".s-"+i+" h2",x:-300, duration: 1.5,opacity:0.2});
+          gsap.from(".s-"+i+" .col-sm-12",{scrollTrigger:".s-"+i+" .col-sm-12",x:-300, duration: 1.5,opacity:0.2});
+
+        }
+        </script>
+    </body>
+</html>
