454ce2dc648d6188cc522a344c4ca83e12627622
shrirangop <shrirangelk221@gmail.com>
2024-03-07 00:29:56 +0530

    logo changed and conflict resolved

--- sparc.php
+++ sparc.php
@@ -7,7 +7,8 @@
     <title>Students' Alumni Cell | SPARC</title>
     <link rel="stylesheet" href="css/sparc.css">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> -->
-    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./flogo.ico" type="image/x-icon" />
+
     <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
