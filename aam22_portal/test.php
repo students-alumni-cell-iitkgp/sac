0f3b3bcc6842810c36daf0bd73a1f79de2c64603
nirmal-2512 <nirmalpatidar2533@gmail.com>
2025-11-16 18:50:38 +0530

    Changed AAM portal added webcover1

--- aam22_portal/test.php
+++ aam22_portal/test.php
@@ -4,8 +4,8 @@
 // $dbname = 'sac_aam22';
 // $username = 'sac_aam';
 // $password = 'sac@aam22';
-$connection = new mysqli("localhost", "root", "", "aam");
-// $connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam22");
+// $connection = new mysqli("localhost", "root", "", "aam");
+$connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam22");
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
