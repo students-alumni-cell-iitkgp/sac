feece17ccf9cfe68ee9ba9ae0cdf6d0977faf3ab
nirmal-2512 <nirmalpatidar2533@gmail.com>
2026-01-02 21:01:10 +0530

    Test Payment integration Successfull

--- aam22_portal/test.php
+++ aam22_portal/test.php
@@ -5,8 +5,8 @@
 // $username = 'sac_aam';
 // $password = 'sac@aam22';
 // $connection = new mysqli("localhost", "root", "", "aam");
-$connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam22");
-// $connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam_test");
+// $connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam22");
+$connection = new mysqli("localhost", "sac_aam", "sac@aam22", "sac_aam_test");
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
